5559292b6afdacb28c081d95f618445fdad7d429
jue131 <83157668+jue131@users.noreply.github.com>
2024-04-18 20:06:58 +0900

    add 6.0 effects to 6-2 & fix bg loop

---
+++ gfx/fancy_graphics/scripts/light_pulse.gd
@@ -0,0 +1,11 @@
+extends PointLight2D
+
+
+func _ready() -> void:
+	await get_tree().create_timer(randf_range(0.05, 0.5), false, false, false).timeout
+	var tw = create_tween().set_loops()
+	tw.tween_property(self, "energy", 0.5, 0.8)
+	tw.tween_property(self, "energy", 1.2, 1.0)
+	var tw2 = create_tween().set_loops()
+	tw2.tween_property(self, "scale", Vector2.ONE * 0.8, 0.8)
+	tw2.tween_property(self, "scale", Vector2.ONE * 1.2, 1.0)
