3b1b6f6aa75c085f3bc1f10d7adc40b07fc02b3e
jue131 <83157668+jue131@users.noreply.github.com>
2024-04-23 01:38:03 +0900

    1-2; fix some music loops

--- gfx/fancy_graphics/scripts/light_pulse.gd
+++ gfx/fancy_graphics/scripts/light_pulse.gd
@@ -1,9 +1,12 @@
 extends PointLight2D
 
-var rand_pause: float = randf_range(0.05, 0.5)
+@export var random_pause: bool = true
+
+var rand_pause: float = randf_range(0.05, 1.7)
 
 func _ready() -> void:
-	await get_tree().create_timer(rand_pause, false, false, false).timeout
+	if random_pause:
+		await get_tree().create_timer(rand_pause, false, false, false).timeout
 	var tw = create_tween().set_loops()
 	tw.tween_property(self, "energy", 0.5, 0.8)
 	tw.tween_property(self, "energy", 1.2, 1.0)
