6fb1333d9fda9430db11a553e76f823c12b13f73
reflexguru <trianglestudioad@gmail.com>
2024-06-28 00:43:29 +0300

    working plushy sun

--- gfx/fancy_graphics/scripts/plushy_sun.gd
+++ gfx/fancy_graphics/scripts/plushy_sun.gd
@@ -1,11 +1,10 @@
 extends Sprite2D
 
+@onready var initial_pos: Vector2 = global_position
+@export var multiplicator: float = 5
+@export var speed: float = 300
+var counter: float
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _physics_process(delta: float) -> void:
+	counter += speed * delta
+	global_position = initial_pos + Vector2(0, sin(deg_to_rad(counter)) * multiplicator)
