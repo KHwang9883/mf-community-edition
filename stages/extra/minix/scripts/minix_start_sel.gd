9e42a96cc7e66fad57f1ab4f33970253b8f928b2
reflexguru <trianglestudioad@gmail.com>
2024-07-23 06:26:37 +0300

    update the selected map in leaderboard on game start

--- stages/extra/minix/scripts/minix_start_sel.gd
+++ stages/extra/minix/scripts/minix_start_sel.gd
@@ -1,8 +1,11 @@
 extends MenuSelection
 
 @onready var node_2d: Node2D = $"../.."
+@onready var control = $"../../../Leaderboard/SubViewportContainer/SubViewport/Control/CanvasLayer/Title"
 
 func _handle_select() -> void:
 	super()
 	get_parent().focused = false
 	node_2d.start_game()
+	control.map_id = node_2d.map_id + 1
+	control._update_map()
