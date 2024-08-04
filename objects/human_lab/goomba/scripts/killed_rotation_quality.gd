cb72603d4a79cf3830a265f7a723f18d28f1c587
jue131 <83157668+jue131@users.noreply.github.com>
2024-08-04 09:01:48 +0900

    hardcore 2-3

--- objects/human_lab/goomba/scripts/killed_rotation_quality.gd
+++
@@ -1,7 +0,0 @@
-extends Sprite2D
-
-func _physics_process(delta: float) -> void:
-	if !visible: return
-	var quality: SettingsManager.QUALITY = SettingsManager.settings.quality
-	if quality == SettingsManager.QUALITY.MIN:
-		rotation += delta * 12.5
