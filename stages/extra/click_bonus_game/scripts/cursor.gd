40dfb5843a02a1fa37dbdb409faf11ac697c485d
jue131 <83157668+jue131@users.noreply.github.com>
2024-08-05 03:37:48 +0900

    fix mouse collisions in bonus levels

--- stages/extra/click_bonus_game/scripts/cursor.gd
+++ stages/extra/click_bonus_game/scripts/cursor.gd
@@ -9,7 +9,7 @@ var hover_count: int = 0
 @onready var timer = $Timer
 @onready var gpu_particles_2d = $GPUParticles2D
 @onready var gpu_particles_2d_2 = $GPUParticles2D2
-
+@onready var area_2d: Area2D = $Area2D
 
 
 func _ready() -> void:
@@ -24,7 +24,14 @@ func _input(event) -> void:
 		return
 	
 	if event is InputEventMouseButton && event.button_index == 1 && event.is_pressed():
-		if !hover_count:
+		var group_stars = get_tree().get_nodes_in_group(&"bonus_star")
+		var has_collision: bool
+		for i in area_2d.get_overlapping_areas():
+			if group_stars.has(i):
+				has_collision = true
+				i.activate()
+				break
+		if !has_collision:
 			gpu_particles_2d.restart()
 			Audio.play_1d_sound(DIE)
 			Scenes.current_scene.tries._try_lost()
