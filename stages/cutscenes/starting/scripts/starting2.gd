b8c713a3adcc549238eb4791d441de2a08b479a0
reflexguru <trianglestudioad@gmail.com>
2024-08-06 04:02:47 +0300

    fix circle transitions

--- stages/cutscenes/starting/scripts/starting2.gd
+++ stages/cutscenes/starting/scripts/starting2.gd
@@ -65,29 +65,24 @@ func _unhandled_input(event: InputEvent):
 
 func _fade_out() -> void:
 	_skippable = false
-	if _crossfade:
-		_restore()
-		ProfileManager.current_profile.data.current_world = goto_scene
-		ProfileManager.save_current_profile()
-		await get_tree().physics_frame
+	
+	_restore()
+	await get_tree().physics_frame
+	
+	if !_crossfade:
+		TransitionManager.accept_transition(
+			load("res://engine/components/transitions/circle_transition/circle_transition.tscn")
+				.instantiate()
+				.with_speeds(0.04, -0.1)
+				.with_pause()
+				.on_player_after_middle(true)
+		)
+		
+		await TransitionManager.transition_middle
+		Scenes.goto_scene(goto_scene)
+	else:
 		TransitionManager.accept_transition(
 			load("res://engine/components/transitions/crossfade_transition/crossfade_transition.tscn")
 				.instantiate()
 				.with_scene(goto_scene)
-				.with_time(0.8)
 		)
-		return
-	TransitionManager.accept_transition(
-		load("res://engine/components/transitions/circle_transition/circle_transition.tscn")
-			.instantiate()
-			.with_speeds(0.01, -0.1)
-	)
-	await TransitionManager.transition_middle
-	
-	_restore()
-	await get_tree().physics_frame
-	await get_tree().physics_frame
-	ProfileManager.current_profile.data.current_world = goto_scene
-	ProfileManager.save_current_profile()
-	Scenes.goto_scene.call_deferred(goto_scene)
-	await Scenes.scene_ready
--- stages/world_8/scripts/level_8-4_boss_level.gd
+++ stages/world_8/scripts/level_8-4_boss_level.gd
@@ -7,20 +7,25 @@ func finish(walking: bool = false, walking_dir: int = 1) -> void:
 
 func throw_to_scene() -> void:
 	await get_tree().create_timer(0.8, false, false).timeout
-			
+	var _crossfade: bool = SettingsManager.get_tweak("replace_circle_transitions_with_fades", false)
+
 	if jump_to_scene:
-		TransitionManager.accept_transition(
-			load("res://engine/components/transitions/circle_transition/circle_transition.tscn")
-				.instantiate()
-				.with_speeds(0.04, -0.1)
-		)
-		
-		TransitionManager.transition_middle.connect(func():
-			TransitionManager.current_transition.paused = true
+		if !_crossfade:
+			TransitionManager.accept_transition(
+				load("res://engine/components/transitions/circle_transition/circle_transition.tscn")
+					.instantiate()
+					.with_speeds(0.04, -0.1)
+					.with_pause()
+					.on_player_after_middle(completion_center_on_player_after_transition)
+			)
+			
+			await TransitionManager.transition_middle
 			Scenes.goto_scene(jump_to_scene)
-			Scenes.scene_changed.connect(func(_current_scene):
-				TransitionManager.current_transition.paused = false
-			, CONNECT_ONE_SHOT)
-		, CONNECT_ONE_SHOT)
+		else:
+			TransitionManager.accept_transition(
+				load("res://engine/components/transitions/crossfade_transition/crossfade_transition.tscn")
+					.instantiate()
+					.with_scene(jump_to_scene)
+			)
 	else:
 		printerr("[Level] Jump to scene is not defined in the level.")
