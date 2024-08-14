c25d36ccbc84efab90d1c9b5b5d3f0199e47ff50
jue131 <83157668+jue131@users.noreply.github.com>
2024-08-15 01:28:03 +0900

    starman running

--- components/secrets_manager/secret_unlocker.gd
+++ components/secrets_manager/secret_unlocker.gd
@@ -1,9 +1,31 @@
 extends Node
 
+@export_category("Unlock Achievement")
 @export var secrets: Array[String] = [""]
 @export var show_toast: bool = true
+@export_category("Progress Achievement")
+@export var progress_by_id: String
+@export var progress_to: int
 
 func unlock_secret(id: int = 0) -> void:
 	if id < len(secrets):
 		if secrets[id].is_empty(): return
 		SecretsManager.set_secret(secrets[id], true, true, show_toast)
+
+
+func progress_secret(id: int = 0) -> void:
+	var new_secret = SecretsManager.get_secret(secrets[id])
+	if typeof(new_secret) == TYPE_BOOL && new_secret == true:
+		return
+	if new_secret == null:
+		new_secret = []
+	if new_secret.has(progress_by_id):
+		return
+	new_secret.append(progress_by_id)
+	var complete: bool
+	if len(new_secret) >= progress_to:
+		complete = true
+		SecretsManager.set_secret(secrets[id], true, false, true)
+		return
+	SecretsManager.set_secret(secrets[id], new_secret, true, false)
+	
