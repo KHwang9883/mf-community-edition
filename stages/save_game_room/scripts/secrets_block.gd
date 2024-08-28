8a785f3751aad2d18fa6e4c83fe374c8f358e5ba
jue131 <83157668+jue131@users.noreply.github.com>
2024-08-28 09:50:56 +0900

    some changes

--- stages/save_game_room/scripts/secrets_block.gd
+++ stages/save_game_room/scripts/secrets_block.gd
@@ -1,8 +1,11 @@
 extends Node2D
 
+signal block_triggered
+
 func _ready() -> void:
 	if SettingsManager.get_tweak("console_enabled", false):
 		show()
 	else:
 		hide()
+		block_triggered.emit()
 		process_mode = Node.PROCESS_MODE_DISABLED
