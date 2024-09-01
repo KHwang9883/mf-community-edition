74e95d7446b3e016a8f240cbac3d2c9b7a6acbae
jue131 <83157668+jue131@users.noreply.github.com>
2024-09-02 05:27:15 +0900

    fix mario minix warp

--- stages/save_game_room/scripts/secrets_block.gd
+++ stages/save_game_room/scripts/secrets_block.gd
@@ -5,7 +5,7 @@ signal block_triggered
 func _ready() -> void:
 	if SettingsManager.get_tweak("console_enabled", false):
 		show()
+		block_triggered.emit()
 	else:
 		hide()
-		block_triggered.emit()
 		process_mode = Node.PROCESS_MODE_DISABLED
