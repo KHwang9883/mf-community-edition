da80b181d03a8e74a36274bd6908a035b712df31
jue131 <83157668+jue131@users.noreply.github.com>
2024-09-01 10:02:01 +0900

    some fixes + start 9-5

--- objects/volcano/bob_omb/bob_omb_instantiater.gd
+++ objects/volcano/bob_omb/bob_omb_instantiater.gd
@@ -5,14 +5,14 @@ extends Node2D
 func _ready() -> void:
 	Thunder.reorder_top(self)
 	await get_tree().physics_frame
-	bob_omb.speed.x = 350
+	bob_omb.speed.x = 150
 	bob_omb.speed_to_dir()
 	bob_omb.collision = false
-	bob_omb.speed.y = -500
+	bob_omb.speed.y = -300
 	bob_omb.global_rotation_degrees = 0
 	global_rotation_degrees = 0
 	var tw = create_tween().set_parallel()
-	tw.tween_property(bob_omb, "speed:x", 50 * bob_omb.dir, 0.9)
+	tw.tween_property(bob_omb, "speed:x", 100 * bob_omb.dir, 0.6)
 	tw.tween_property(bob_omb, "global_rotation_degrees", 0, 0.01)
 	await get_tree().create_timer(0.1, false, true).timeout
 	if !is_instance_valid(bob_omb):
