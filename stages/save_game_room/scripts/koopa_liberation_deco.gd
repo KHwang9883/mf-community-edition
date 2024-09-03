90a6b3b97aa1183bffa2b5ffa09035c61d1a9f03
jue131 <83157668+jue131@users.noreply.github.com>
2024-09-03 19:22:37 +0900

    world of stupidity level 1

--- stages/save_game_room/scripts/koopa_liberation_deco.gd
+++ stages/save_game_room/scripts/koopa_liberation_deco.gd
@@ -4,5 +4,5 @@ func _physics_process(delta: float) -> void:
 	var player: Player = Thunder._current_player
 	if player:
 		var facing: float
-		facing = Thunder.Math.look_at(global_position, player.global_position, global_transform)
+		facing = Thunder.Math.look_at(global_position + Vector2(16, 0), player.global_position, global_transform)
 		flip_h = (facing < 0 && facing != 0)
