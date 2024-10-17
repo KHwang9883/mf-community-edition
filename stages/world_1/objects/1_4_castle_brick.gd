bb474353163a2ec078e06ffc2f31e158eb2f700c
reflexguru <trianglestudioad@gmail.com>
2024-10-17 15:02:35 +0300

    implement lava end cutscene

--- stages/world_1/objects/1_4_castle_brick.gd
+++ stages/world_1/objects/1_4_castle_brick.gd
@@ -2,6 +2,7 @@ extends StaticBody2D
 
 @export var do_rotate: bool = true
 @export var rotate_fixed: float = 0.0
+@export var delay: float = 0
 @onready var init_speed_y: float = randf_range(120, 150)
 @onready var rotation_speed: float = randf_range(-12, 12)
 
@@ -17,6 +18,9 @@ func _physics_process(delta: float) -> void:
 
 
 func set_falling() -> void:
+	if delay > 0:
+		await get_tree().create_timer(delay, false).timeout
+	
 	falling = true
 	collision_layer = 0
 	if rotate_fixed:
