ac104eb6ab9a18a043820ee9e622b007c89ac93b
jue131 <83157668+jue131@users.noreply.github.com>
2024-11-09 19:15:46 +0900

    fix hc1 & hc2 complete maps

--- stages/save_game_room/scripts/pipe_save_with_selector.gd
+++ stages/save_game_room/scripts/pipe_save_with_selector.gd
@@ -91,6 +91,7 @@ func delete_save() -> void:
 	Audio.play_1d_sound(preload("res://engine/objects/bumping_blocks/_sounds/break.wav"))
 	_star_world = false
 	_update_reset_labels()
+	$CursedPipe.visible = false
 
 
 func pass_warp() -> void:
