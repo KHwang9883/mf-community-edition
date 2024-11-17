5f897ac3ab80544bc5f35a1bc29731650660036c
jue131 <83157668+jue131@users.noreply.github.com>
2024-11-18 05:05:45 +0900

    something ig

--- stages/main_menu/scripts/camera_tweaks_menu.gd
+++ stages/main_menu/scripts/camera_tweaks_menu.gd
@@ -4,8 +4,10 @@ extends "res://engine/scenes/main_menu/scripts/opt_camera_2d.gd"
 
 func _ready() -> void:
 	super()
+	@warning_ignore("narrowing_conversion")
 	limit_bottom = (margin * 2) + menu_controller.size.y
 
 
 func update_limit() -> void:
+	@warning_ignore("narrowing_conversion")
 	limit_bottom = (margin * 2) + menu_controller.size.y
