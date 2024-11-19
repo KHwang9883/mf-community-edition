a429cf59ed10d5243aa0a5d339265d114e046773
jue131 <83157668+jue131@users.noreply.github.com>
2024-11-20 06:11:23 +0900

    fix: story label; skin selector desc; min lamp offset

--- stages/cutscenes/starting/scripts/story_label.gd
+++ stages/cutscenes/starting/scripts/story_label.gd
@@ -5,6 +5,7 @@ var speed = 6
 
 func _ready() -> void:
 	modulate.a = 0
+	text = text % CharacterManager.get_character_display_name()
 
 func _physics_process(delta: float) -> void:
 	global_position.y -= speed * delta
