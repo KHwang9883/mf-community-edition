680a57a269bc4e0bdc09da7272f767611d7fe5de
jue131 <83157668+jue131@users.noreply.github.com>
2024-11-26 13:49:11 +0900

    fix console_patch in main menu

--- stages/main_menu/scripts/console_patch.gd
+++ stages/main_menu/scripts/console_patch.gd
@@ -1,6 +1,6 @@
 extends Node
 
 func _physics_process(delta: float) -> void:
-	var tweak: bool = SecretsManager.is_console_enabled()
+	var tweak: bool = SettingsManager.get_tweak("console_enabled", false)
 	if Console.is_visible() && !tweak:
 		Console.hide()
