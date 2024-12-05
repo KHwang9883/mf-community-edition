3f08807e361e9e1b28827a3b78876e4d68336952
jue131 <83157668+jue131@users.noreply.github.com>
2024-12-06 06:35:42 +0900

    fix starting cutscenes not skippable with controller

--- stages/cutscenes/starting/scripts/starting2.gd
+++ stages/cutscenes/starting/scripts/starting2.gd
@@ -59,7 +59,7 @@ func _physics_process(_delta: float) -> void:
 
 func _unhandled_input(event: InputEvent):
 	if !_skippable: return
-	if event is InputEventKey:
+	if event is InputEventKey || event is InputEventJoypadButton:
 		_fade_out()
 
 func _fade_out() -> void:
