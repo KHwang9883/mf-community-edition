5f7251942a14e9a31b8b5c6cb2a56baea111c099
jue131 <83157668+jue131@users.noreply.github.com>
2024-12-21 14:34:37 +0900

    expert mode 1-1 & 1-2

--- stages/world_1/scripts/1-1_pipe_secret.gd
+++ stages/world_1/scripts/1-1_pipe_secret.gd
@@ -1,6 +1,7 @@
 extends Node
 
 const LIFE_MUSHROOM = preload("res://engine/objects/powerups/life_mushroom/life_mushroom.tscn")
+const POISONOUS_MUSHROOM = preload("res://engine/objects/powerups/poisonous_mushroom/poisonous_mushroom.tscn")
 @onready var par = $".."
 @onready var text: Sprite2D = $"../../HUD/Sprite2D"
 
@@ -21,6 +22,15 @@ func _spawn_lives() -> void:
 		scene.add_child(life)
 		life.global_position = i.global_position + Vector2(0, -16)
 		life.reset_physics_interpolation()
+	
+	var poison_spawns = scene.get_node_or_null("PoisonSpawns")
+	if is_instance_valid(poison_spawns):
+		for i in poison_spawns.get_children():
+			var life = POISONOUS_MUSHROOM.instantiate()
+			life.appear_distance = 0
+			scene.add_child(life)
+			life.global_position = i.global_position + Vector2(0, -16)
+			life.reset_physics_interpolation()
 	
 	text.modulate.a = 1.0
 	var tw = create_tween().bind_node(text)
