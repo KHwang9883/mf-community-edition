dc7cf590002bfbb7645f4fdaf02d2794cd12c191
jue131 <83157668+jue131@users.noreply.github.com>
2024-12-21 16:28:41 +0900

    fix expert save again; reset lives on expert 1-2 warp

--- stages/extra/expert_mode/scripts/world_4_warp.gd
+++ stages/extra/expert_mode/scripts/world_4_warp.gd
@@ -1,10 +1,15 @@
 extends "res://stages/world_1/scripts/text_secret_passage.gd"
 
-const expert_1_1 := &"res://stages/world_1/expert_level_1-1.tscn"
+const expert_1_1 := "res://stages/world_1/expert_level_1-1.tscn"
+const expert_1_1_str := &"res://stages/world_1/expert_level_1-1.tscn"
 
 func activate() -> void:
 	if ProfileManager.current_profile.has_completed(expert_1_1):
 		ProfileManager.current_profile.data.completed_levels.erase(expert_1_1)
+	if ProfileManager.current_profile.has_completed(expert_1_1_str):
+		ProfileManager.current_profile.data.completed_levels.erase(expert_1_1_str)
 	if ProfileManager.current_profile.data.get("star_world") && "map_force_selected_marker" in Data.values:
 		Data.values.map_force_selected_marker = ""
+	if Data.values.lives > 4:
+		Data.values.lives = 4
 	super()
