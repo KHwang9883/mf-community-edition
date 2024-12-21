5ae05b263dadbc20094eb5ee10aa75233de4ddb0
jue131 <83157668+jue131@users.noreply.github.com>
2024-12-22 00:51:28 +0900

    broken 4-2

---
+++ stages/extra/expert_mode/scripts/world_4_2_checkpoint.gd
@@ -0,0 +1,10 @@
+extends "res://engine/objects/core/checkpoint/checkpoint.gd"
+
+@onready var path_follow_2d = $"../Path2D6/PathFollow2D"
+
+func _ready():
+	if Data.values.checkpoint == id:
+		path_follow_2d.progress_ratio = 1.0
+		path_follow_2d.speed = -100
+	await get_tree().physics_frame
+	super()
