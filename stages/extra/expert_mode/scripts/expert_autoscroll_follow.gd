87a76032015c88148131a738938564704a17353b
jue131 <83157668+jue131@users.noreply.github.com>
2024-12-23 13:06:45 +0900

    finish 4-2 expert mode: enemies & checkpoint progression

--- stages/extra/expert_mode/scripts/expert_autoscroll_follow.gd
+++ stages/extra/expert_mode/scripts/expert_autoscroll_follow.gd
@@ -8,6 +8,7 @@ var notified = false
 @onready var node_extra_enemies: Node2D = %NodeExtraEnemies
 @onready var node_no_checkpoint: Node2D = %NodeNoCheckpoint
 @onready var no_cp: bool = Data.values.checkpoint == -1
+@onready var player_camera_2d: Camera2D = $PlayerCamera2D
 
 
 func _ready() -> void:
@@ -19,14 +20,23 @@ func _physics_process(delta: float) -> void:
 	super(delta)
 	if speed < -1 && !notified:
 		notified = true
-		finish_line.position.y = 416
+		player_camera_2d.enable_left_border_death = false
+		player_camera_2d.enable_right_border_death = true
+		for i in Scenes.current_scene.get_children():
+			if i is GravityBody2D && is_instance_valid(i) && !i is Player:
+				i.queue_free.call_deferred()
+		
 		node_extra_enemies.process_mode = Node.PROCESS_MODE_INHERIT
 		node_extra_enemies.visible = true
 		node_extra_enemies.position.y = 0
-		if no_cp:
-			node_no_checkpoint.process_mode = Node.PROCESS_MODE_INHERIT
-			node_no_checkpoint.visible = true
-			node_extra_enemies.position.y = 0
+		#if no_cp:
+		node_no_checkpoint.process_mode = Node.PROCESS_MODE_INHERIT
+		node_no_checkpoint.visible = true
+		node_no_checkpoint.position.y = 0
+	
+		get_tree().create_timer(0.5, false).timeout.connect(func():
+			finish_line.position.y = 416
+		)
 
 
 func set_speed(new_speed: float) -> void:
