ff1bec340cd0f4aad7db937825d517d90ecf5ad5
jue131 <83157668+jue131@users.noreply.github.com>
2025-01-23 22:09:12 +0900

    implement shader compilation on game startup

--- stages/intro/scripts/intro.gd
+++ stages/intro/scripts/intro.gd
@@ -1,13 +1,39 @@
 extends Control
 
+const SHADER_CACHER = preload("res://stages/intro/shader_cacher.tscn")
+
 @onready var disclaimer: TextureRect = $CenterContainer/TextureRect
+@onready var icon: Sprite2D = $Icon
+var loading_finished: bool = false
+var loading_init: bool = false
 
 func _ready() -> void:
+	SettingsManager.enable_shortcut_scene_change_keys = false
+	await get_tree().create_timer(0.6, false, true, false).timeout
+	print("[Startup] Compiling shaders...")
+	var cacher = SHADER_CACHER.instantiate()
+	Scenes.current_scene.add_child(cacher)
+	Thunder.reorder_top(cacher)
+	loading_init = true
+
+
+func _physics_process(delta: float) -> void:
+	if loading_init && !loading_finished:
+		loading_finished = true
+		print("[Startup] Waiting for a bit..")
+		get_tree().create_timer(0.6, false, true, false).timeout.connect(display_disclaimer)
+		print("[Startup] Shader compilation complete!")
+
+
+func display_disclaimer() -> void:
+	icon.queue_free()
 	var tw = disclaimer.create_tween()
-	tw.tween_property(disclaimer, "modulate:a", 1.0, 0.8)
+	tw.tween_property(disclaimer, "modulate:a", 1.0, 0.6)
 	tw.tween_interval(2.2)
-	tw.tween_property(disclaimer, "modulate:a", 0.0, 0.8)
+	tw.tween_property(disclaimer, "modulate:a", 0.0, 0.6)
 	tw.tween_callback(transition)
+	await get_tree().physics_frame
+	SettingsManager.enable_shortcut_scene_change_keys = true
 
 
 func transition() -> void:
