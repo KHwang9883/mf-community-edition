2cbb333031f330b5d17be8e7654d6bd7cf28ac66
jue131 <83157668+jue131@users.noreply.github.com>
2025-01-31 16:17:49 +0900

    tweaks: description changes; incomplete warning for revamps

--- stages/extra/hardcore_1/scripts/tweaked_marker_path.gd
+++ stages/extra/hardcore_1/scripts/tweaked_marker_path.gd
@@ -3,7 +3,12 @@ extends Node
 @export_file("*.tscn", "*.scn") var new_level_path: String
 
 @onready var _tweak: bool = SettingsManager.get_tweak("remade_levels", true)
+@onready var _warn_tweak: bool = SettingsManager.get_tweak("show_warning_on_revamped_levels", true)
+@onready var revamp_warning: CanvasLayer = $RevampWarning
 
 func _ready() -> void:
 	if new_level_path && _tweak:
+		if _warn_tweak:
+			revamp_warning.toggle()
+			return
 		get_parent().level = new_level_path
