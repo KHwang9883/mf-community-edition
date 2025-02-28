149e1a32327815fc35c152d6781dee9819e2e0af
jue131 <83157668+jue131@users.noreply.github.com>
2025-03-01 06:07:49 +0900

    fix atom star sprite position

--- objects/_screen_clear_item/atom_star.gd
+++ objects/_screen_clear_item/atom_star.gd
@@ -7,6 +7,7 @@ func collect() -> void:
 		ScoreText.new(str(score), self)
 		Data.values.score += score
 	
+	var worked: bool
 	for i in get_tree().get_nodes_in_group(&"end_level_sequence"):
 		if i is Projectile:
 			if i.belongs_to == Data.PROJECTILE_BELONGS.PLAYER:
@@ -19,11 +20,13 @@ func collect() -> void:
 		
 		if i.killing_enabled:
 			i.got_killed("boomerang", [], false)
+			worked = true
 
 	var powerup_sfx = CharacterManager.get_sound_replace(pickup_powerup_sound, DEFAULT_POWERUP_SOUND, "powerup", true)
 	var neutral_sfx = CharacterManager.get_sound_replace(pickup_neutral_sound, DEFAULT_NEUTRAL_SOUND, "powerup", true)
 	
-	Audio.play_sound(powerup_sfx, self, false, {pitch = sound_pitch, ignore_pause = true})
-
-	Audio.play_sound(neutral_sfx, self, false, {pitch = sound_pitch})
+	if worked:
+		Audio.play_sound(powerup_sfx, self, false, {pitch = sound_pitch, ignore_pause = true})
+	else:
+		Audio.play_sound(neutral_sfx, self, false, {pitch = sound_pitch})
 	queue_free()
