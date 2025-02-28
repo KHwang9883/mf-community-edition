be63eb8ac09b3654ce5b0606b3486fd3a39b9470
jue131 <83157668+jue131@users.noreply.github.com>
2025-03-01 05:01:00 +0900

    expert goomba party; atom star; fix world 2 ending

---
+++ stages/extra/expert_mode/scripts/goomba_party_expert.gd
@@ -0,0 +1,96 @@
+extends Node2D
+
+@export var goto_scene: String
+
+@onready var lostmap_title_mario: Sprite2D = $LostmapTitleMario
+@onready var label: RichTextLabel = $Label
+@onready var color_rect: ColorRect = $CanvasLayer/ColorRect
+@onready var super_mario: Sprite2D = $SuperMario
+@onready var super_mario_2: Sprite2D = $SuperMario2
+@onready var pause: Label = $Pause
+@onready var marker_2d: Marker2D = $Marker2D
+@onready var marker_2d_2: Marker2D = $Marker2D2
+@onready var music_loader: Node = $MusicLoader
+@onready var control: Control = $CanvasLayer/Control
+@onready var video_stream_player: VideoStreamPlayer = $CanvasLayer/Control/VideoStreamPlayer
+
+const POWERUP = preload("res://engine/objects/players/prefabs/sounds/powerup.wav")
+
+var can_start: bool = false
+var pause_tw: Tween
+
+func _ready() -> void:
+	label.modulate.a = 0
+	lostmap_title_mario.modulate.a = 0
+	pause.modulate.a = 0
+	control.modulate.a = 0
+	var level_to_compl = "res://stages/world_1/expert_level_1-3.tscn"
+	if !ProfileManager.current_profile.has_completed(level_to_compl):
+		ProfileManager.current_profile.complete_level(level_to_compl)
+		ProfileManager.save_current_profile()
+	$CanvasLayer/Control/Label.text = "coming\n" + str(Time.get_datetime_dict_from_system().year + 1)
+	
+	await get_tree().create_timer(1, false).timeout
+	can_start = true
+	await get_tree().create_timer(3, false).timeout
+	var tw = create_tween()
+	tw.tween_property(lostmap_title_mario, "modulate:a", 1, 2)
+	
+	await tw.finished
+	_label_fader()
+
+func _label_fader() -> void:
+	var tw = create_tween()
+	tw.tween_property(label, "modulate:a", 1, 2)
+
+
+func _physics_process(delta: float) -> void:
+	super_mario.position = super_mario.position.move_toward(marker_2d.position, delta * 350)
+	super_mario_2.position = super_mario_2.position.move_toward(marker_2d_2.position, delta * 350)
+	
+	if Input.is_action_just_pressed(&"pause_toggle"):
+		if pause_tw: pause_tw.kill()
+		pause.modulate.a = 1.0
+		pause_tw = pause.create_tween()
+		pause_tw.tween_interval(1.0)
+		pause_tw.tween_property(pause, ^"modulate:a", 0.0, 0.6)
+	
+	
+	if can_start && Input.is_action_just_pressed("ui_accept"):
+		can_start = false
+		Audio.play_1d_sound(POWERUP)
+		
+		await get_tree().create_timer(1.5, false).timeout
+		Audio.stop_music_channel(1, true)
+		
+		var tw = create_tween()
+		tw.tween_property(color_rect, "modulate:a", 1, 1)
+		tw.tween_interval(0.5)
+		tw.tween_property(control, "modulate:a", 1.0, 1.5)
+		tw.tween_interval(1.0)
+		tw.tween_callback(video_stream_player.play)
+		video_stream_player.finished.connect(start_transition)
+		
+func start_transition() -> void:
+	var _crossfade: bool = SettingsManager.get_tweak("replace_circle_transitions_with_fades", false)
+	
+	if !_crossfade:
+		TransitionManager.accept_transition(
+			load("res://engine/components/transitions/circle_transition/circle_transition.tscn")
+				.instantiate()
+				.with_speeds(0.04, -0.1)
+				.with_pause()
+		)
+		
+		await TransitionManager.transition_middle
+		Scenes.goto_scene(goto_scene)
+	else:
+		TransitionManager.accept_transition(
+			load("res://engine/components/transitions/crossfade_transition/crossfade_transition.tscn")
+				.instantiate()
+				.with_scene(goto_scene)
+		)
+
+
+func _on_timer_timeout() -> void:
+	$HBoxContainer.visible = !$HBoxContainer.visible
