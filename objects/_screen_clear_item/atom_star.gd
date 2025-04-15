6149860aa40b3933bbca710d058ed68113be1015
jue131 <83157668+jue131@users.noreply.github.com>
2025-04-15 11:44:27 +0900

    feat: item stock functionality

--- objects/_screen_clear_item/atom_star.gd
+++ objects/_screen_clear_item/atom_star.gd
@@ -1,32 +1,13 @@
 extends Powerup
 
-
+@export var item_name: String = "atom"
 
 func collect() -> void:
 	if score > 0:
 		ScoreText.new(str(score), self)
 		Data.add_score(score)
 	
-	var worked: bool
-	for i in get_tree().get_nodes_in_group(&"end_level_sequence"):
-		if i is Projectile:
-			if i.belongs_to == Data.PROJECTILE_BELONGS.PLAYER:
-				continue
-			i.queue_free()
-			continue
-		if !i.get(&"_center"): continue
-		if !Thunder.view.is_getting_closer(i._center, 32):
-			continue
-		
-		if i.killing_enabled:
-			i.got_killed("boomerang", [], false)
-			worked = true
-
-	var powerup_sfx = CharacterManager.get_sound_replace(pickup_powerup_sound, DEFAULT_POWERUP_SOUND, "powerup", true)
-	var neutral_sfx = CharacterManager.get_sound_replace(pickup_neutral_sound, DEFAULT_NEUTRAL_SOUND, "powerup", true)
+	Data.values.item = item_name
 	
-	if worked:
-		Audio.play_sound(powerup_sfx, self, false, {pitch = sound_pitch, ignore_pause = true})
-	else:
-		Audio.play_sound(neutral_sfx, self, false, {pitch = sound_pitch})
+	Audio.play_sound(pickup_powerup_sound, self, false, {pitch = sound_pitch, ignore_pause = true})
 	queue_free()
---
+++ objects/antiafk_expert_mode/item_scripts/item_atom.gd
@@ -0,0 +1,29 @@
+extends TextureRect
+
+const DEFAULT_POWERUP_SOUND = preload("res://engine/objects/players/prefabs/sounds/powerup.wav")
+const INCORRECT = preload("res://sfx/incorrect.wav")
+
+func activate() -> bool:
+	var worked: bool
+	for i in get_tree().get_nodes_in_group(&"end_level_sequence"):
+		if i is Projectile:
+			if i.belongs_to == Data.PROJECTILE_BELONGS.PLAYER:
+				continue
+			i.queue_free()
+			continue
+		if !i.get(&"_center"): continue
+		if !Thunder.view.is_getting_closer(i._center, 32):
+			continue
+		
+		if i.killing_enabled:
+			i.got_killed("boomerang", [], false)
+			worked = true
+
+	var powerup_sfx = CharacterManager.get_sound_replace(DEFAULT_POWERUP_SOUND, DEFAULT_POWERUP_SOUND, "powerup", true)
+
+	if worked:
+		Audio.play_1d_sound(powerup_sfx, false, {ignore_pause = true})
+	else:
+		Audio.play_1d_sound(INCORRECT, false, {ignore_pause = true})
+	
+	return worked
