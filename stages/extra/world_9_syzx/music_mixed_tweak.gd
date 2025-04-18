c98432e140c9c44c8515d1cca41a09979d68e359
jue131 <83157668+jue131@users.noreply.github.com>
2025-04-18 09:34:09 +0900

    feat: pitch changing tweak; fixes

--- stages/extra/world_9_syzx/music_mixed_tweak.gd
+++ stages/extra/world_9_syzx/music_mixed_tweak.gd
@@ -5,6 +5,7 @@ extends Node
 func _ready() -> void:
 	var bgm_tweak: int = SettingsManager.get_tweak("bgm_as_in_version", 0)
 	if bgm_tweak == 0:
-		get_parent().index = 0 if is_advanced else 1
+		$"..".index = 0 if is_advanced else 1
 		
-	get_parent().play_buffered.call_deferred()
+	$"..".music_started.emit($"..".index)
+	$"..".play_buffered.call_deferred()
