68560769d4d8074b05148c1273406f33c3f29861
jue131 <83157668+jue131@users.noreply.github.com>
2025-04-28 00:03:13 +0900

    feat: quick suit settings VERY WIP

--- stages/main_menu/objects/character_editor/skin_suit_category_sel.gd
+++ stages/main_menu/objects/character_editor/skin_suit_category_sel.gd
@@ -4,6 +4,17 @@ extends "res://stages/main_menu/scripts/tweak_category_sel.gd"
 
 
 func _handle_select(mouse_input: bool = false) -> void:
+	if SkinsManager.current_skin && SkinsManager.suit_tweaks.has(SkinsManager.current_skin):
+		get_tree().call_group(&"_skin_suit_tweak", &"queue_free")
+		#for i in SkinsManager.suit_tweaks[SkinsManager.current_skin]:
+			#print(i)
+		#for i in move_to.get_children():
+		#	if !i is MenuSelection: continue
+		#	if "tweak_name" in i:
+		#		i.is_toggled = i.tweak_name
+	#elif !SkinsManager.current_skin:
+		#return
+		
 	super(mouse_input)
 	move_to.set_meta("_powerup_name", powerup_name)
 	var _label = move_to.get_child(0).get_child(0)
@@ -12,9 +23,4 @@ func _handle_select(mouse_input: bool = false) -> void:
 			_label.set_meta("orig_text", _label.text)
 		_label.text = _label.get_meta("orig_text", "%s") % powerup_name
 	
-	SkinsManager.suit_tweaks
-	
-	for i in move_to.get_children():
-		if !i is MenuSelection: continue
-		if "tweak_name" in i:
-			i.is_toggled = i.tweak_name
+	move_to._update_selectors()
--- stages/main_menu/scripts/tweak_category_sel.gd
+++ stages/main_menu/scripts/tweak_category_sel.gd
@@ -11,6 +11,8 @@ extends MenuSelection
 @onready var selector_to: MenuSelector = get_node_or_null(menu_selector_path)
 var _timer: float
 
+signal selection_entered
+
 func _physics_process(delta: float) -> void:
 	super(delta)
 	if !valu:
@@ -24,12 +26,17 @@ func _physics_process(delta: float) -> void:
 
 func _handle_select(mouse_input: bool = false) -> void:
 	super(mouse_input)
+	selection_entered.emit()
 	get_parent().position.x = 1000
 	get_parent().reset_physics_interpolation()
 	move_to.position.x = 168
 	move_to.reset_physics_interpolation()
 	get_parent().focused = false
 	Scenes.current_scene.get_node("Tweaks/Label2").visible = false
+	camera_2d.menu_controller = move_to
+	camera_2d.selector = selector_to
+	camera_2d.update_limit()
+	camera_2d.reset_physics_interpolation()
 	await get_tree().physics_frame
 	
 	get_parent().move_selector(reset_to, true)
@@ -37,7 +44,3 @@ func _handle_select(mouse_input: bool = false) -> void:
 	get_parent().focused = false
 	if show_desc_bool:
 		Scenes.current_scene.get_node("Tweaks/Label2").visible = true
-	camera_2d.menu_controller = move_to
-	camera_2d.selector = selector_to
-	camera_2d.update_limit()
-	camera_2d.reset_physics_interpolation()
