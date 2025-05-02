4ad488a2af1d307381ec87dd2848d28aaac8b4f9
jue131 <83157668+jue131@users.noreply.github.com>
2025-05-03 04:30:01 +0900

    tweak selection improvements; expert world 7 emalpka battle; fix expert 7-1 & 8-1 death music bug

--- stages/extra/expert_mode/scripts/world_8_music_loop.gd
+++ stages/extra/expert_mode/scripts/world_8_music_loop.gd
@@ -1,9 +1,26 @@
 extends Node
 
+var is_zero: bool = false
+@onready var old_death_music
+@onready var old_death_stop_music
+@onready var old_wait_time
+
 func _ready() -> void:
 	await get_tree().physics_frame
 	var pl: Player = $".."
 	if Data.values.lives == 0 && pl.death_check_for_lives:
+		is_zero = true
+		old_death_music = pl.death_music_override
+		old_death_stop_music = pl.death_stop_music
+		old_wait_time = pl.death_wait_time
 		pl.death_music_override = null
 		pl.death_stop_music = true
 		pl.death_wait_time = 3.5
+
+func _physics_process(delta: float) -> void:
+	if is_zero && Data.values.lives > 0:
+		var pl: Player = $".."
+		is_zero = false
+		pl.death_music_override = old_death_music
+		pl.death_stop_music = old_death_stop_music
+		pl.death_wait_time = old_wait_time
--- stages/save_game_room/scripts/progress_continue_skipper.gd
+++ stages/save_game_room/scripts/progress_continue_skipper.gd
@@ -24,6 +24,7 @@ func set_regular_edition() -> void:
 func mario_forever_advance(is_from_save: bool = false) -> void:
 	SettingsManager.set_tweak("life_every_2_mil_score", false)
 	SettingsManager.set_tweak("stomping_combo", false)
+	SettingsManager.set_tweak("super_jump_bug", false)
 	SettingsManager.set_tweak("harder_level_design", true)
 	SettingsManager.set_tweak("minigames_in_main_worlds", true)
 	if !is_from_save:
