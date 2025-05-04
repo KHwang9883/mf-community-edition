af3c0237a2cc19b7de5027fec63d06e0f01d19b4
looper231 <cooler2311@gmail.com>
2025-05-04 17:40:33 +0400

    otherworld 6-2: almost finished

---
+++ gfx/rain/rain.gd
@@ -0,0 +1,6 @@
+extends GPUParticles2D
+
+@export_category("Rain")
+@export_group("Sounds", "sound_")
+@export_range(0, 60, 0.001, "suffix:s") var sound_far_lightning_interval_base: float = 12
+@export_range(0, 60, 0.001, "suffix:s") var sound_far_lightning_interval_extra: float = 18
