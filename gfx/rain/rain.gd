7d83f6fe6e64995a6cddbcecf1abf4eed85b38ab
jue131 <83157668+jue131@users.noreply.github.com>
2025-05-09 23:07:05 +0900

    feat: skin error checker; OW6-2 one-way launchers; expert 4-1 wip

--- gfx/rain/rain.gd
+++ gfx/rain/rain.gd
@@ -4,3 +4,14 @@ extends GPUParticles2D
 @export_group("Sounds", "sound_")
 @export_range(0, 60, 0.001, "suffix:s") var sound_far_lightning_interval_base: float = 12
 @export_range(0, 60, 0.001, "suffix:s") var sound_far_lightning_interval_extra: float = 18
+
+@onready var sound: AudioStreamPlayer = $Sound
+
+func _ready() -> void:
+	SettingsManager.settings_updated.connect(_update_visibility, CONNECT_DEFERRED)
+	_update_visibility.call_deferred()
+
+
+func _update_visibility() -> void:
+	sound.playing = is_visible_in_tree()
+	
