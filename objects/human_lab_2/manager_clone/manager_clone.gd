6949335538a3cae83c72e5a7002606980210d788
jue131 <83157668+jue131@users.noreply.github.com>
2025-05-13 10:01:38 +0900

    feat: mutant clone, manager clone

---
+++ objects/human_lab_2/manager_clone/manager_clone.gd
@@ -0,0 +1,47 @@
+extends GeneralMovementBody2D
+
+const ATTACK = preload("res://objects/human_lab_2/manager_clone/sfx/attack.ogg")
+const MANAGER_PROJECTILE = preload("res://objects/human_lab_2/manager_clone/manager_projectile.tscn")
+
+@onready var timer: Timer = $Timer
+@onready var timer_shooting: Timer = $TimerShooting
+
+var bullets_left: int
+
+func _on_timer_timeout() -> void:
+	var pl: Player = Thunder._current_player
+	if !pl: return
+	
+	sprite_node.animation = "shoot"
+	sprite_node.frame = 4
+	speed.x = 0
+	turn_sprite = false
+	update_dir()
+	sprite_node.flip_h = dir < 0
+	
+	bullets_left = 3
+	timer.start(3.6)
+	timer_shooting.start(0.11)
+
+
+func _on_timer_shooting_timeout() -> void:
+	if bullets_left == 0:
+		sprite_node.play("default")
+		speed.x = 50
+		speed_to_dir()
+		turn_sprite = true
+		return
+	
+	var bullet = MANAGER_PROJECTILE.instantiate()
+	bullet.global_transform = global_transform
+	bullet.position.y += 4
+	Scenes.current_scene.add_child(bullet)
+	bullet.reset_physics_interpolation()
+	
+	sprite_node.play("shoot")
+	update_dir()
+	sprite_node.flip_h = dir < 0
+	
+	bullets_left -= 1
+	Audio.play_sound(ATTACK, self, false)
+	timer_shooting.start(0.11)
---
+++ objects/human_lab_2/manager_clone/manager_projectile.gd
@@ -0,0 +1,25 @@
+extends Projectile
+
+@export var angle_precision_deg: float = 11.25
+@export var remove_offscreen_after: float = 1.0
+
+func _ready() -> void:
+	offscreen_handler(remove_offscreen_after)
+	super()
+	var pl: Player = Thunder._current_player
+	if !pl: return
+	
+	look_at(pl.global_position)
+	var ANGLE: float = angle_precision_deg * (PI / 180.0)
+	global_rotation = ANGLE * roundf(global_rotation / ANGLE)
+	speed = speed.rotated(global_rotation)
+
+
+func _on_level_end() -> void:
+	if !Thunder.view.is_getting_closer(self, 32):
+		if Thunder.view.is_getting_closer(self, 320):
+			queue_free()
+		return
+	Data.add_score(100)
+	ScoreText.new(str(100), self)
+	queue_free()
