8390371fbdfc1d66f58426ef96f5808633b9f7cc
jue131 <83157668+jue131@users.noreply.github.com>
2025-06-21 21:06:26 +0900

    fix emalpka checker; update sound test; ci: include compat bats

--- stages/main_menu/objects/character_editor/audio_test_selection.gd
+++ stages/main_menu/objects/character_editor/audio_test_selection.gd
@@ -3,47 +3,52 @@ extends MenuSelection
 @onready var valu = get_node_or_null(^"Value")
 #var _timer: float
 var sounds_arr: Array
-var snd_selected_id: int = -1
+var snd_is_selected: bool = false
 var snd_total_ids: int
+var snd_next_id: int
 
 func _ready() -> void:
-	if valu: valu.modulate.a = 0.0
+	#if valu: valu.modulate.a = 0.0
 	snd_total_ids = sounds_arr.size()
+	if snd_total_ids != 0:
+		snd_next_id = snd_total_ids - 1
+	_update_text()
 
 func _handle_focused(focus: bool) -> void:
 	focused = focus
-	if !focus: return
-	valu.modulate.a = 1.0
+	if !focus && selected_sound: return
+	#valu.modulate.a = 1.0
 	_update_text()
 
 func _physics_process(delta: float) -> void:
 	super(delta)
 	
-	if !valu:
-		return
-	if !focused: valu.modulate.a = 0.0
+	#if !valu:
+	#	return
+	#if !focused: valu.modulate.a = 0.0
 
 
 func _handle_select(mouse_input: bool = false) -> void:
-	get_tree().set_group(&"_sounds", "snd_selected_id", -1)
+	get_tree().call_group(&"_sounds", &"set_selected_false")
 	get_tree().call_group(&"preview_sound", &"queue_free")
-	snd_selected_id = 0
+	snd_is_selected = true
 	if !sounds_arr.is_empty():
-		snd_selected_id = randi_range(0, len(sounds_arr) - 1)
-		selected_sound = sounds_arr[snd_selected_id]
+		#snd_selected_id = randi_range(0, len(sounds_arr) - 1)
+		snd_next_id = wrapi(snd_next_id + 1, 0, len(sounds_arr))
+		selected_sound = sounds_arr[snd_next_id]
 		
 	super(mouse_input)
 	if !selected_sound:
-		snd_selected_id = -1
+		snd_is_selected = false
 		return
 	_update_text()
 
 func _sound_finished() -> void:
-	snd_selected_id = -1
+	snd_is_selected = false
 	_update_text()
 
 func _update_text() -> void:
-	if snd_selected_id == -1:
+	if !snd_is_selected:
 		if sounds_arr.is_empty():
 			valu.text = "default"
 			if !selected_sound:
@@ -51,7 +56,7 @@ func _update_text() -> void:
 			return
 		valu.text = "total: %d" % [snd_total_ids]
 	else:
-		valu.text = "playing %d / %d" % [snd_selected_id + 1, max(snd_total_ids, 1)]
+		valu.text = "playing %d / %d" % [snd_next_id + 1, max(snd_total_ids, 1)]
 
 func _play_sound():
 	if !selected_sound:
@@ -65,3 +70,9 @@ func _play_sound():
 	snd.process_mode = Node.PROCESS_MODE_ALWAYS
 	snd.play.call_deferred()
 	
+
+func set_selected_false() -> void:
+	var _do_update: bool = snd_is_selected
+	snd_is_selected = false
+	if _do_update:
+		_update_text()
