240b05ab624b2f810b657ae39ebd5189e46001fc
jue131 <83157668+jue131@users.noreply.github.com>
2025-07-08 15:42:50 +0900

    move BTE 9-4 => 9-5; feat wip BTE 9-4

---
+++ stages/extra/world_9/scripts/9_5_rumble.gd
@@ -0,0 +1,49 @@
+extends Node
+
+const INTRO_CASTLE_CRUSH_2 = preload("res://sfx/IntroCastleCrush2.wav")
+#@onready var color_rect: ColorRect = $"../../LavaAnim/ColorRect"
+@onready var collision_shape_2d: CollisionShape2D = $"../../LavaAnim/Area2D/CollisionShape2D"
+@onready var lava_anim: Node2D = $"../../LavaAnim"
+@onready var glow: Node2D = $"../../LavaAnim/Right/Glow"
+@onready var lava_top_hud: Sprite2D = $"../../HUD/LavaTopHUD"
+@onready var lava_hud: AnimatedSprite2D = $"../../HUD/LavaHUD"
+
+var lava_speed: float
+
+func _player_triggered() -> void:
+	Audio.play_1d_sound(INTRO_CASTLE_CRUSH_2, false, {volume = -3})
+	var cam: PlayerCamera2D = Thunder._current_camera
+	if !cam: return
+	cam.shock_smooth(12, 12)
+
+func start_moving() -> void:
+	collision_shape_2d.set_deferred(&"disabled", false)
+	var tw = create_tween().set_process_mode(Tween.TWEEN_PROCESS_PHYSICS).set_trans(Tween.TRANS_CUBIC).set_parallel()
+	#tw.tween_property(color_rect, "size:x", 896, 3.0)
+	#tw.tween_property(color_rect, "position:x", 0, 3.0)
+	tw.tween_property(self, "lava_speed", 70, 0.5)
+	tw.tween_property(glow, "scale:y", 1.0, 2.0)
+	if Thunder._current_player:
+		tw.tween_property(lava_top_hud, "modulate:a", 1.0, 1.6)
+		tw.tween_property(lava_hud, "modulate:a", 1.0, 1.6)
+
+func _physics_process(delta: float) -> void:
+	if lava_speed <= 0: return
+	var cam: PlayerCamera2D = Thunder._current_camera
+	if cam:
+		if cam.get_screen_center_position().x + 380 < lava_anim.global_position.x + 896:
+			lava_speed = 0
+			return
+	if lava_anim.global_position.x > 9248:
+		lava_speed = 0
+		return
+	lava_anim.position.x += lava_speed * delta
+	lava_speed = minf(lava_speed + 8 * delta, 275)
+	var player: Player = Thunder._current_player
+	if is_instance_valid(player):
+		lava_hud.position.y = lava_top_hud.position.y - ((lava_anim.global_position.x + 1400) - player.global_position.x) / 40
+
+func warped() -> void:
+	var tw = create_tween().set_parallel()
+	tw.tween_property(lava_hud, "modulate:a", 0, 2)
+	tw.tween_property(lava_top_hud, "modulate:a", 0, 2)
--- stages/extra/world_9/scripts/canvas_modulate.gd
+++ stages/extra/world_9/scripts/canvas_modulate.gd
@@ -1,47 +1,47 @@
 extends CanvasModulate
 
-var _has_faded: bool
-var disabled: bool
+#var _has_faded: bool
+#var disabled: bool
 
-@onready var background: Sprite2D = $"../CanvasLayer/Node2D/background"
-@onready var point_light_2d: PointLight2D = $"../Mario/PointLight2D"
+#@onready var background: Sprite2D = $"../CanvasLayer/Node2D/background"
+#@onready var point_light_2d: PointLight2D = $"../Mario/PointLight2D"
 
 func _ready() -> void:
 	visible = true
 
-func _physics_process(delta: float) -> void:
-	var module
-	var audio_node: AudioStreamPlayer
-	if !Audio._music_channels.has(1): return
-	if (
-		is_instance_valid(Audio._music_channels[1]) &&
-		Audio._music_channels[1].has_meta(&"openmpt")
-	):
-		audio_node = Audio._music_channels[1]
-		module = Audio._music_channels[1].get_meta(&"openmpt")
+#func _physics_process(delta: float) -> void:
+	#var module
+	#var audio_node: AudioStreamPlayer
+	#if !Audio._music_channels.has(1): return
+	#if (
+		#is_instance_valid(Audio._music_channels[1]) &&
+		#Audio._music_channels[1].has_meta(&"openmpt")
+	#):
+		#audio_node = Audio._music_channels[1]
+		#module = Audio._music_channels[1].get_meta(&"openmpt")
 	
-	if _null_check(module): return
-	if disabled:
-		audio_node.process_mode = Node.PROCESS_MODE_ALWAYS
-		return
-	var pat = module.get_current_pattern()
+	#if _null_check(module): return
+	#if disabled:
+		#audio_node.process_mode = Node.PROCESS_MODE_ALWAYS
+		#return
+	#var pat = module.get_current_pattern()
 	
-	audio_node.process_mode = Node.PROCESS_MODE_DISABLED if Scenes.custom_scenes.pause.opened else Node.PROCESS_MODE_ALWAYS
+	#audio_node.process_mode = Node.PROCESS_MODE_DISABLED if Scenes.custom_scenes.pause.opened else Node.PROCESS_MODE_ALWAYS
 	
-	if pat == 4 && !_has_faded:
-		_has_faded = true
-		color.v = 0.4
-		await get_tree().create_timer(0.4, false, false, true).timeout
-		disabled = true
-		var tw = create_tween().set_parallel()
-		var time_scaled: float = 1.2 * Engine.time_scale
-		tw.tween_property(background, "modulate:a", 0.8, time_scaled)
-		tw.tween_property(point_light_2d, "energy", 0.0, time_scaled)
-		tw.tween_property(self, "color", Color(0.7, 0.7, 0.7), time_scaled)
+	#if pat == 4 && !_has_faded:
+		#_has_faded = true
+		#color.v = 0.4
+		#await get_tree().create_timer(0.4, false, false, true).timeout
+		#disabled = true
+		#var tw = create_tween().set_parallel()
+		#var time_scaled: float = 1.2 * Engine.time_scale
+		#tw.tween_property(background, "modulate:a", 0.8, time_scaled)
+		#tw.tween_property(point_light_2d, "energy", 0.0, time_scaled)
+		#tw.tween_property(self, "color", Color(0.7, 0.7, 0.7), time_scaled)
 
 
-
-func _null_check(module: Node) -> bool:
-	if !module: return true
-	if !is_instance_valid(module): return true
-	return false
+#
+#func _null_check(module: Node) -> bool:
+	#if !module: return true
+	#if !is_instance_valid(module): return true
+	#return false
