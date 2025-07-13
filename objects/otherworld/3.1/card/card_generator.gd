e17bbd94ade0bbe27a69ba824a3d1762977dd584
jue131 <83157668+jue131@users.noreply.github.com>
2025-07-13 22:34:07 +0900

    wip otherworld level 6

---
+++ objects/otherworld/3.1/card/card_generator.gd
@@ -0,0 +1,24 @@
+extends Node2D
+
+const CARD = preload("res://objects/otherworld/3.1/card/card.tscn")
+const STAKE = preload("res://objects/otherworld/3.1/card/stake.wav")
+const SPEEDS = [-110, -70, 70, 110]
+
+@onready var sprite: Sprite2D = $Deck/Sprite
+
+var order: int
+
+func _ready() -> void:
+	order = randi_range(0, SPEEDS.size() - 1)
+	sprite.texture.region.position.x = 71 * order
+
+func _on_timer_timeout() -> void:
+	Audio.play_sound(STAKE, self, false)
+	NodeCreator.prepare_2d(CARD, self).create_2d().bind_global_transform(Vector2(0, 8)).call_method(
+		func(card: Projectile):
+			card.belongs_to = Data.PROJECTILE_BELONGS.ENEMY
+			card.speed = Vector2(SPEEDS[order], -150)
+			card.sprite_node.texture.region.position.x = 71 * order
+	)
+	order = wrapi(order + 1, 0, SPEEDS.size())
+	sprite.texture.region.position.x = 71 * order
---
+++ objects/otherworld/3.1/cursor/cursor.gd
@@ -0,0 +1,63 @@
+extends Node2D
+
+signal scripted_move_ended
+signal scripted_move_waiting
+
+const BOO_SOUND = preload("res://objects/boo/boo1.wav")
+const SMOKE = preload("res://engine/objects/effects/smoke/smoke.tscn")
+
+@export var speed: float = 50
+@export var scripted_move: bool = false
+@export var scripted_wait_to_end_sec: float = 0.6
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+var _moving_scripted: bool
+var _marker_pos: Vector2
+var _moving_script_timer: float
+
+func _ready() -> void:
+	if scripted_move:
+		_marker_pos = $Marker2D.global_position
+		_moving_scripted = true
+
+func _physics_process(delta: float) -> void:
+	if _moving_scripted:
+		sprite.animation = &"chase"
+		global_position = global_position.move_toward(
+			_marker_pos,
+			speed * (delta * 2)
+		)
+		if global_position.is_equal_approx(_marker_pos):
+			if _moving_script_timer == 0:
+				scripted_move_waiting.emit()
+			_moving_script_timer += delta
+			if _moving_script_timer > scripted_wait_to_end_sec:
+				_moving_scripted = false
+				scripted_move_ended.emit()
+		return
+	
+	var player: Player = Thunder._current_player
+	if !player:
+		sprite.play(&"default")
+		return
+	
+	var dir := signi(Thunder.Math.look_at(global_position, player.global_position, global_transform))
+
+	if player.direction == dir:
+		sprite.animation = &"chase"
+		global_position = global_position.move_toward(
+			player.global_position,
+			speed * (delta * 2)
+		)
+	else:
+		sprite.play(&"default")
+		
+
+func _from_bumping_block() -> void:
+	Audio.play_sound(BOO_SOUND, self)
+	position += Vector2(0, -24).rotated(global_rotation)
+	z_index += 2
+	
+	var smoke = SMOKE.instantiate()
+	Scenes.current_scene.add_child(smoke)
+	smoke.global_position = global_position
