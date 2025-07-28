feadb3668ae3e7f5e7300c02c29713031b4f3b73
jue131 <83157668+jue131@users.noreply.github.com>
2025-07-28 21:18:57 +0900

    first part of expert 6-4 done

--- stages/extra/world_12/scripts/12-2_toxic_fixer.gd
+++ stages/extra/world_12/scripts/12-2_toxic_fixer.gd
@@ -1,5 +1,13 @@
 extends Node
 
+var locked: bool = false
+
+func _ready() -> void:
+	if Data.values.checkpoint == -1:
+		locked = false
 
 func _on_cam_area_2_view_section_changed() -> void:
-	pass # Replace with function body.
+	if locked: return
+	locked = true
+	
+	
