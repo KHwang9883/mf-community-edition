e3ca84f06c8d44251ea47c150ff6e049e15125a1
jue131 <83157668+jue131@users.noreply.github.com>
2025-08-21 00:31:35 +0900

    fix syzx w10 progression; better spelling in save room messages; update snowland_red tileset texture

--- stages/extra/hardcore_1/scripts/tweaked_marker_path.gd
+++ stages/extra/hardcore_1/scripts/tweaked_marker_path.gd
@@ -1,6 +1,7 @@
 extends Node
 
 @export_file("*.tscn", "*.scn") var new_level_path: String
+@export var set_data_only: bool = false
 
 @onready var _tweak: bool = SettingsManager.get_tweak("remade_levels", true)
 @onready var _warn_tweak: bool = SettingsManager.get_tweak("show_warning_on_revamped_levels", true)
@@ -12,7 +13,12 @@ var is_current: bool
 func _ready() -> void:
 	if !new_level_path: return
 	if _tweak && !_warn_tweak:
+		if set_data_only:
+			Data.values.revamp_scene = Scenes.get_scene_path(new_level_path)
+			return
 		get_parent().level = Scenes.get_scene_path(new_level_path)
+	if "revamp_scene" in Data.values:
+		Data.values.erase("revamp_scene")
 
 func _physics_process(delta: float) -> void:
 	if !_warn_tweak: return
@@ -29,9 +35,14 @@ func prepare_warning() -> void:
 	Scenes.current_scene.enter_on_request_only = true
 	Scenes.current_scene.player_entered_level.connect(revamp_warning.toggle)
 	revamp_warning.selected_new.connect(func() -> void:
-		get_parent().level = Scenes.get_scene_path(new_level_path)
+		if set_data_only:
+			Data.values.revamp_scene = Scenes.get_scene_path(new_level_path)
+		else:
+			get_parent().level = Scenes.get_scene_path(new_level_path)
 		Scenes.current_scene.enter_level_sequence()
 	)
 	revamp_warning.selected_old.connect(func() -> void:
 		Scenes.current_scene.enter_level_sequence()
+		if "revamp_scene" in Data.values:
+			Data.values.erase("revamp_scene")
 	)
