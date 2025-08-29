18fbd075cb1172cc9927aeb6739ad5f1e0b5b499
jue131 <83157668+jue131@users.noreply.github.com>
2025-08-29 18:34:10 +0900

    expert mode ending credits

--- stages/extra/expert_mode/ending_scene/ending_credits.gd
+++ stages/extra/expert_mode/ending_scene/ending_credits.gd
@@ -4,10 +4,13 @@ extends Node2D
 @onready var color_rect: ColorRect = $Canvas/ColorRect
 var _original_time_scale: float
 var skippable: bool = false
+var skippable_extra: bool = false
 var _crossfade: bool = SettingsManager.get_tweak("replace_circle_transitions_with_fades", false)
 @onready var credits: Control = $Canvas/Credits
+@onready var peach: AnimatedSprite2D = $Node2D/peach
+@onready var thanks: RichTextLabel = $Canvas/thanks
 
-var counter: float
+var counter: float = -1.0
 
 @onready var node_2d: Node2D = $Node2D
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
@@ -16,6 +19,7 @@ func _ready() -> void:
 	color_rect.visible = true
 	credits.visible = false
 	credits.modulate.a = 0.0
+	
 	var tw = color_rect.create_tween()
 	tw.tween_property(color_rect, "color:a", 0.0, 3.0)
 	animation_player.play(&"new_animation")
@@ -24,12 +28,30 @@ func _ready() -> void:
 	print("set skippable")
 
 func _physics_process(delta: float) -> void:
-	_flow_intros(delta)
+	if counter < 0: return
+	counter += delta * 20
+	credits.position.y = -counter
+	if credits.position.y < -2144:
+		skippable_extra = true
+	if credits.position.y <= -2900:
+		counter = -99
+	if thanks.position.y > 0:
+		thanks.position.y = credits.position.y + 2432
+	else:
+		thanks.position.y = 0
 
-func _flow_intros(delta: float) -> void:
-	counter += delta
-	if counter > 1:
-		pass
+func end_sequence() -> void:
+	credits.visible = true
+	var camera_2d = $Camera2D
+	#animation_player.stop(true)
+	var tw = create_tween()
+	tw.tween_property(peach, "modulate:a", 0.0, 0.5)
+	tw.tween_interval(1.0)
+	tw.tween_property(camera_2d, "position:y", -720, 5.0).set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_SINE)
+	tw.tween_property(credits, "modulate:a", 1.0, 2.0)
+	tw.tween_interval(1.5)
+	await tw.finished
+	counter = 0
 
 func _enter_tree() -> void:
 	print('[Cutscene] altered time scale from %s' % Engine.time_scale)
@@ -43,6 +65,10 @@ func _restore() -> void:
 func _unhandled_input(event: InputEvent):
 	if !skippable: return
 	if event.is_action_pressed(&"ui_cancel"):
+		_start_transition()
+	if !skippable_extra: return
+	
+	if event.is_action_pressed(&"m_jump"):
 		_start_transition()
 
 func _start_transition() -> void:
