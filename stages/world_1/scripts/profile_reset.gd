65f8208970c56e9bf232fece66e79d1162c20f5e
jue131 <83157668+jue131@users.noreply.github.com>
2025-08-30 13:32:31 +0900

    bump to 2.0.1; disable item shop in OW lvl 8 boss; fix save pipe corruption after hardcore warp; feat ice flower in save room

--- stages/world_1/scripts/profile_reset.gd
+++ stages/world_1/scripts/profile_reset.gd
@@ -1,6 +1,12 @@
 extends Node
 
+@export var new_profile: bool = false
+
 func profile_reset() -> void:
+	if new_profile:
+		ProfileManager.create_new_profile(&"debug")
+		return
+	
 	Data.values.goto_human_lab = "true"
 	var profile = ProfileManager.current_profile
 	var path = Scenes.current_scene.scene_file_path
@@ -8,4 +14,3 @@ func profile_reset() -> void:
 		profile.complete_level(path)
 		ProfileManager.save_current_profile()
 	
-	#ProfileManager.create_new_profile(&"debug")
