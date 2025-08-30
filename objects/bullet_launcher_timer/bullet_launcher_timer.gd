4d26ee8f9200fcb8cafaa12a358a1a73e3ad4261
jue131 <83157668+jue131@users.noreply.github.com>
2025-08-31 01:50:29 +0900

    feat hammer item in exp6-3; fix a bullet launcher in exp7-4

--- objects/bullet_launcher_timer/bullet_launcher_timer.gd
+++ objects/bullet_launcher_timer/bullet_launcher_timer.gd
@@ -4,6 +4,8 @@ extends "res://engine/objects/enemies/bullet_bill/launcher/bullet_launcher.gd"
 @export var shoot_interval_modifier: float = 1
 @export var shoot_interval_offset: float = 0
 @export var use_getting_closer: bool = true
+@onready var left: Sprite2D = $CanvasGroup/Left
+@onready var right: Sprite2D = $CanvasGroup/Right
 
 func _ready() -> void:
 	animation_player.speed_scale = shoot_interval_modifier
@@ -12,3 +14,18 @@ func _ready() -> void:
 func _launch_bullet() -> void:
 	if !Thunder.view.is_getting_closer(self, 32) && use_getting_closer: return
 	_on_bullet_launched()
+
+func _create_bullet() -> NodeCreator.NodeCreation:
+	reset_anim()
+	return super()
+
+func stop_anim() -> void:
+	left.visible = false
+	right.visible = false
+
+func reset_anim() -> void:
+	left.visible = true
+	right.visible = true
+
+func _on_screen_entered() -> void:
+	return
