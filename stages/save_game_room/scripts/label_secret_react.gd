652eaeef4f2eaa47a453f89a376c23eaaeb2167d
jue131 <83157668+jue131@users.noreply.github.com>
2025-09-08 22:47:31 +0900

    feat: syzx world level selectors; fix regular syzx worlds not showing kevin colored labels [no ci]

--- stages/save_game_room/scripts/label_secret_react.gd
+++ stages/save_game_room/scripts/label_secret_react.gd
@@ -1,15 +1,16 @@
-extends Label
+extends "res://engine/scenes/save_game_room/scripts/saved_level_label.gd"
 
 @export var secret_name: String
 @export var secret_progress_id: String
 @export var secret_kevin_name: String
 
 func _ready() -> void:
+	super()
 	var secret = SecretsManager.get_secret(secret_name)
 	if secret_kevin_name:
 		var secr_kev = SecretsManager.get_secret(secret_kevin_name)
 		if secr_kev && typeof(secr_kev) == TYPE_ARRAY && secret_progress_id in secr_kev:
-			add_theme_color_override(&"font_color", Color("#a755ff"))
+			add_theme_color_override(&"font_color", Color("#b16dff"))
 			return
 			
 	if typeof(secret) != TYPE_ARRAY:
