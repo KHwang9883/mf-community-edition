35bd6d7296778f15f5f3e9558cf0686d58da9389
jue131 <83157668+jue131@users.noreply.github.com>
2025-09-13 01:35:14 +0900

    feat: update checker in save room (for dumb people)

--- stages/main_menu/scripts/update_modal_sel.gd
+++ stages/main_menu/scripts/update_modal_sel.gd
@@ -1,9 +1,10 @@
 extends MenuSelection
 
 @export var cancel: bool = true
+@export_node_path("Node") var update_checker_path = ^"../../../../General/UpdateChecker"
 
 @onready var pause: Control = $"../.."
-@onready var update_checker: Node = $"../../../../General/UpdateChecker"
+@onready var update_checker: Node = get_node_or_null(update_checker_path)
 
 func _handle_select(mouse_input: bool = false) -> void:
 	super(mouse_input)
