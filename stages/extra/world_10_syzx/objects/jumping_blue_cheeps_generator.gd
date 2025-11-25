eef1945468c47df17f3fc381a4b208200e0ee0f3
jue131 <83157668+jue131@users.noreply.github.com>
2025-11-25 16:49:33 +0900

    feat: syzx world 12-2 [no ci]

--- stages/extra/world_10_syzx/objects/jumping_blue_cheeps_generator.gd
+++ stages/extra/world_10_syzx/objects/jumping_blue_cheeps_generator.gd
@@ -3,6 +3,10 @@ extends "res://engine/objects/enemies/cheeps/jumping_cheeps_generator.gd"
 const CHEEP_YEL_JUMPING = preload("res://stages/extra/world_10_syzx/objects/cheep_yel_jumping.tscn")
 
 @export var chance_every_stopped_sec: float = 0.2
+@export var replace_by_yellow_in_advanced: bool = true
+@export var random_spawn_min := Vector2i(32, 16)
+@export var random_spawn_max := Vector2i(32, 316)
+@export var destroy_below_y: float = 560
 @onready var _tweak = ProfileManager.current_profile.data.get("advanced_edition", false)
 
 func _time() -> void:
@@ -16,7 +20,13 @@ func _time() -> void:
 	if get_tree().get_node_count_in_group("obj_by_" + str(get_instance_id())) >= max_on_screen:
 		return
 	Thunder.view.cam_border()
-	var to_pos = Vector2(Thunder.view.border.end) + Vector2(32, randi_range(16, 316))
+	var to_pos = Vector2(Thunder.view.border.end) + Vector2(
+		randi_range(random_spawn_min.x, random_spawn_max.x),
+		randi_range(random_spawn_min.y, random_spawn_max.y)
+	)
+	
+	if to_pos.y > 560:
+		return
 	
 	var fish
 	if !_tweak:
@@ -27,5 +37,6 @@ func _time() -> void:
 	fish.reset_physics_interpolation()
 	fish.speed.x = randi_range(speed_min.x, speed_max.x)
 	fish.speed.y = randi_range(speed_min.y, speed_max.y)
+	fish.life_time = 1.0
 	fish.add_to_group("obj_by_" + str(get_instance_id()))
 	Scenes.current_scene.add_child(fish)
