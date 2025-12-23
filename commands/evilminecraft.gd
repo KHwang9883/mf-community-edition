b5ad75aa5e60c1f2461954f93e5f6d9e3fd3e3eb
jue131 <83157668+jue131@users.noreply.github.com>
2025-12-23 22:39:52 +0900

    feat(console): mineinfdev; fix(console): memleak in minethecraft; fix(ex5-3 kevin): wrong activation area of spike ceiling

--- commands/evilminecraft.gd
+++ commands/evilminecraft.gd
@@ -2,13 +2,13 @@ extends Command
 
 
 static func register() -> Command:
-	return new().set_name("evilminecraft").set_description("Grab your enemies on the go. (Requires Minecraft Mode)")
+	return new().set_name("evilminecraft").set_description("Grab your enemies on the go. (Requires Mine The Craft Mode)")
 
 func execute(args:Array) -> Command.ExecuteResult:
 	if !Console.cv.get("mc_enemy_mode", false):
 		var gui = Scenes.custom_scenes.get("MinecraftGUI")
 		if !is_instance_valid(gui):
-			return Command.ExecuteResult.new("[color=red]Error[/color]: Either you are not on a level, or Minecraft Mode is not activated: [b]minethecraft[/b].")
+			return Command.ExecuteResult.new("[color=red]Error[/color]: Either you are not on a level, or Mine The Craft Mode is not activated: [b]minethecraft[/b].")
 		Console.cv.mc_enemy_mode = true
 		gui.activate_enemy_mode()
 		return Command.ExecuteResult.new("You can now break enemies using the mouse.")
