1702d253ea3755a8e2c7681d9aa4e34e5da9b26b
jue131 <83157668+jue131@users.noreply.github.com>
2025-12-28 01:57:43 +0900

    fix borked exp map 1; fix minix hud loop offsets

--- stages/extra/minix/animated_sprite_2d.gd
+++ stages/extra/minix/animated_sprite_2d.gd
@@ -1,13 +1,19 @@
 extends AnimatedSprite2D
 
+const LoopOffsetBehaviorScript = preload("uid://cdwp13k80fepv")
+
+var loop_offset_behavior_script: GDScript
+
 func fade_out(duration: float = 0.5) -> void:
 	var tw = create_tween()
 	tw.tween_property(self, "self_modulate:a", 0.0, duration)
-	
 
 
 func _ready() -> void:
-	sprite_frames = SkinsManager.apply_player_skin(CharacterManager.get_suit("small", "" if SettingsManager.settings.skin else "Mario"))
+	var _suit = CharacterManager.get_suit("small", "" if SettingsManager.settings.skin else "Mario")
+	sprite_frames = SkinsManager.apply_player_skin(_suit)
+	loop_offset_behavior_script = ByNodeScript.activate_script(LoopOffsetBehaviorScript, self, {suit = _suit.name})
+	
 	play(&"walk")
 
 func _physics_process(delta: float) -> void:
