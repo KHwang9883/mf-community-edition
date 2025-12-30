9c7f5e775c61d8f1b1ba1262b591739f53627be6
Furret78 <68061718+furret78@users.noreply.github.com>
2025-12-30 13:32:46 +0100

    mf flash: added kevin to title [no ci]

--- stages/mario_forever_flash/scripts/flash_title_stage.gd
+++ stages/mario_forever_flash/scripts/flash_title_stage.gd
@@ -2,61 +2,50 @@ extends Stage2D
 
 @export var goto_scene: String
 
-@onready var node_2d: Node2D = $ParallaxBackground/Node2D
-#@onready var label: Label = $ParallaxBackground/Label
 @onready var color_rect: ColorRect = $CanvasLayer/ColorRect
 @onready var controls: MenuItemsController = $MenuLayer/Controls
 @onready var selector: MenuSelector = $MenuLayer/Selector
 
 const POWERUP = preload("res://engine/objects/players/prefabs/sounds/powerup.wav")
 
-#@onready var initial_label_y = label.global_position.y
-
 var label_text_pointer: int = 0
 
 @onready var mario_sprite: AnimatedSprite2D = $MarioSprite
+@onready var kevin_sprite: AnimatedSprite2D = $KevinSprite
 @onready var game_logo: Sprite2D = $Logo2
 
 @onready var screen_camera: Camera2D = $Camera2D
 var decoration_offset: float = 586.0
+var tw_scroll: Tween
 
 func _ready() -> void:
-	mario_sprite.global_position.x = screen_camera.global_position.x + decoration_offset + screen_camera.offset.x
-	mario_sprite.reset_physics_interpolation()
-	game_logo.global_position.x = screen_camera.global_position.x + decoration_offset + screen_camera.offset.x
-	game_logo.reset_physics_interpolation()
+	if !KevinGlobal.activated and is_instance_valid(kevin_sprite):
+		kevin_sprite.queue_free()
+	else:
+		kevin_sprite.sprite_frames = SkinsManager.apply_player_skin(CharacterManager.get_suit("small"))
+		kevin_sprite.play(&"walk", 2.0)
+	
+	_set_obj_position(true)
 	
 	controls.modulate.a = 0
 	selector.modulate.a = 0
-	#lostmap_title_press_enter._min_a = 0.7
 	mario_sprite.sprite_frames = SkinsManager.apply_player_skin(CharacterManager.get_suit("small"))
 	mario_sprite.play(&"walk", 2.0)
 	
-	var tw_scroll = create_tween()
-	tw_scroll.tween_property(self, "decoration_offset", 0.0, 5.0)
+	tw_scroll = create_tween()
+	tw_scroll.tween_property(self, "decoration_offset", 0.0, 7.0)
 	
-	await get_tree().create_timer(5.0, false).timeout
+	await get_tree().create_timer(6.0, false).timeout
 	
+	if controls.focused: return
+	_menu_fade_in()
+	await get_tree().create_timer(0.8, false).timeout
+	controls.focused = true
+
+func _menu_fade_in() -> void:
 	var tw = create_tween().set_parallel()
 	tw.tween_property(controls, "modulate:a", 1, 0.8)
 	tw.tween_property(selector, "modulate:a", 1, 0.8)
-	
-	await get_tree().create_timer(0.8, false).timeout
-	controls.focused = true
-	
-	#await tw.finished
-	#_label_fader()
-#
-#func _label_fader() -> void:
-	#var tw = create_tween()
-	#tw.tween_property(label, "modulate:a", 1, 2)
-	#await get_tree().create_timer(4, false).timeout
-	#
-	#tw = create_tween()
-	#tw.tween_property(label, "modulate:a", 0, 2)
-	#await tw.finished
-	#
-	#_label_fader()
 
 func start_selected() -> void:
 	if !controls.focused: return
@@ -70,9 +59,6 @@ func start_selected() -> void:
 	tw.tween_property(color_rect, "modulate:a", 1, 1)
 	Audio.stop_music_channel(2, true)
 	await tw.finished
-	
-	#ProfileManager.current_profile.data.current_world = goto_scene
-	#ProfileManager.save_current_profile()
 	
 	var _crossfade: bool = SettingsManager.get_tweak("replace_circle_transitions_with_fades", false)
 	
@@ -95,5 +81,23 @@ func start_selected() -> void:
 
 func _physics_process(delta: float) -> void:
 	screen_camera.offset.x += 4.0
+	_set_obj_position()
+
+func _set_obj_position(reset_interp: bool = false) -> void:
+	if is_instance_valid(kevin_sprite):
+		kevin_sprite.global_position.x = screen_camera.global_position.x + decoration_offset + screen_camera.offset.x - 112.0
 	mario_sprite.global_position.x = screen_camera.global_position.x + decoration_offset + screen_camera.offset.x
-	game_logo.global_position.x = screen_camera.global_position.x + decoration_offset + screen_camera.offset.x
+	game_logo.global_position.x = screen_camera.global_position.x + decoration_offset + screen_camera.offset.x - 16.0
+	
+	if reset_interp:
+		if is_instance_valid(kevin_sprite): kevin_sprite.reset_physics_interpolation()
+		mario_sprite.reset_physics_interpolation()
+		game_logo.reset_physics_interpolation()
+
+func _input(event: InputEvent) -> void:
+	if !controls.focused and event.is_action_pressed("ui_accept", true):
+		if is_instance_valid(tw_scroll):
+			tw_scroll.kill()
+			decoration_offset = 0.0
+		_menu_fade_in()
+		controls.focused = true
