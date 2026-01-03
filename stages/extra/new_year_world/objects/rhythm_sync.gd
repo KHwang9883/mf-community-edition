09e54c01ce44b18df78f9dc5f9acf7e86aa438d7
jue131 <83157668+jue131@users.noreply.github.com>
2026-01-04 01:54:26 +0900

    fix crash in ny-1

--- stages/extra/new_year_world/objects/rhythm_sync.gd
+++ stages/extra/new_year_world/objects/rhythm_sync.gd
@@ -11,26 +11,19 @@ func _physics_process(delta: float) -> void:
 	energy = move_toward(energy, 0.1, delta * 4)
 	
 	if odd_frame: return
-	var mod: AudioStreamMPT
 	if !Audio._music_channels.has(1): return
-	if (is_instance_valid(Audio._music_channels[1]) &&
-		Audio._music_channels[1].stream is AudioStreamMPT):
-		mod = Audio._music_channels[1].stream
-	if _null_check(mod): return
-	var playback: AudioStreamPlaybackMPT = Audio._music_channels[1].get_stream_playback()
+	if !is_instance_valid(Audio._music_channels[1]) || !Audio._music_channels[1].playing: return
+	if !Audio._music_channels[1].stream: return
+	var playback = Audio._music_channels[1].get_stream_playback()
 	#var pattern: int = playback.get_current_pattern()
-	var row: int = playback.get_current_row()
-	if row == old_row: return
-	old_row = row
-	if row % 8 == 0:
-		_syncevent()
+	if playback is AudioStreamPlaybackMPT:
+		var row: int = playback.get_current_row()
+		if row == old_row: return
+		old_row = row
+		if row % 8 == 0:
+			_syncevent()
 
 func _syncevent():
 	energy = 2
 	color.s = 0.85
 	color.h = randf_range(0, 1)
-
-func _null_check(mod) -> bool:
-	if !mod: return true
-	if !is_instance_valid(mod): return true
-	return false
