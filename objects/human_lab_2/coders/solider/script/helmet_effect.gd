7c7a1fffd764183e332e4ba7c061dffc64eed324
jue131 <83157668+jue131@users.noreply.github.com>
2026-01-08 22:30:12 +0900

    remove broken script [no ci]

--- objects/human_lab_2/coders/solider/script/helmet_effect.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-const KEYBOARD_EFFECT = preload("res://new_clones/coders/solider/helmet_effect.tscn")
-
-func create() -> void:
-	var keyboard = KEYBOARD_EFFECT.instantiate()
-	Scenes.current_scene.add_child(keyboard)
-	keyboard.position = global_position
-	keyboard.reset_physics_interpolation()
