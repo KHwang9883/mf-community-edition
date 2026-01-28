1f7ab3d4a9c4696c6d8ed6f86566f8aace778835
Usered238 <30762980+Usered238@users.noreply.github.com>
2026-01-29 01:23:11 +0900

    fix atom replenisher ignoring skin sounds

--- objects/ability_atom_replenisher/atom_replenisher.gd
+++ objects/ability_atom_replenisher/atom_replenisher.gd
@@ -10,6 +10,6 @@ func collect() -> void:
 	Data.values.item = item_name
 	Data.technical_values.custom_saved_values.item_replenisher = item_name
 	
-	var _sfx = CharacterManager.get_sound_replace(pickup_powerup_sound, pickup_powerup_sound, "powerup_no_transform", false)
+	var _sfx = CharacterManager.get_sound_replace(pickup_powerup_sound, pickup_powerup_sound, "powerup_no_transform", true)
 	Audio.play_1d_sound(_sfx, false, {pitch = sound_pitch, ignore_pause = true})
 	queue_free()
