3ceeead4631e062ca12d5c4ec69a5abdce3d4726
Usered238 <30762980+Usered238@users.noreply.github.com>
2026-05-23 21:53:25 +0900

    feat(squario): final cutscene; fix 2-2 bonus platforms

--- stages/extra/squario/world_4/final_cutscene.gd
+++ stages/extra/squario/world_4/final_cutscene.gd
@@ -1,7 +1,38 @@
 extends Node
 
-const CASTLE_SMOKE = preload("res://engine/scenes/castle_cutscene/objects/castle_smoke.tscn")
+const EXPLOSION_TANK = preload("res://stages/cutscenes/ending/part_1/scripts/explosion_tank.tscn")
 const JEBUT = preload("uid://2wvrmeop3e7w")
+
+var spr_dict: Dictionary[Texture2D, Rect2i] = {
+	preload("res://engine/objects/enemies/buzzle_bettle/textures/buzzle_beetle.png"):
+		Rect2i(0,0,31,32),
+	preload("res://engine/objects/enemies/goombas/textures/goomba.png"):
+		Rect2i(0,0,31,32),
+	preload("res://engine/objects/enemies/koopas/textures/koopa_green.png"):
+		Rect2i(32,0,32,48),
+	preload("res://engine/objects/enemies/koopas/textures/koopa_red.png"):
+		Rect2i(32,0,32,48),
+	preload("res://engine/objects/enemies/spinies/textures/spiny_red.png"):
+		Rect2i(0,32,32,32),
+	preload("res://engine/objects/enemies/spinies/textures/spiny_egg_red.png"):
+		Rect2i(0,0,31,31),
+	preload("res://engine/objects/enemies/lakitus/textures/lakitu.png"):
+		Rect2i(0,0,31,48),
+	preload("res://engine/objects/enemies/bullet_bill/bill/textures/bullet_bill.png"):
+		Rect2i(0,0,34,28),
+	preload("res://engine/objects/enemies/podoboo/podoboo.png"):
+		Rect2i(32,0,32,32),
+	preload("res://engine/objects/enemies/koopas/textures/shell_green.png"):
+		Rect2i(32,0,32,28),
+	preload("res://engine/objects/enemies/koopas/textures/shell_red.png"):
+		Rect2i(32,0,32,28),
+	preload("res://engine/objects/projectiles/hammer/texture.png"):
+		Rect2i(0,0,24,33),
+	preload("res://engine/objects/enemies/hammer_bros/textures/green_bro.png"):
+		Rect2i(0,0,33,48),
+	preload("res://engine/objects/enemies/piranha_plants/textures/head_green.png"):
+		Rect2i(32,0,32,32),
+}
 
 @export_multiline var credits_text: PackedStringArray
 
@@ -13,11 +44,14 @@ const JEBUT = preload("uid://2wvrmeop3e7w")
 @onready var logo_2: Sprite2D = $"../CanvasLayer/Logo2"
 @onready var control: Control = $"../CanvasLayer/Control"
 @onready var label: Label = $"../CanvasLayer/Control/Label"
+@onready var sprite_holder: Node2D = $"../SpriteHolder"
 
 var _flashed: bool
 var _destroying: bool = false
 var _castle_speed: float = -5
 var _label_index: int = 0
+var sprites: Array[Sprite2D]
+var skippable: bool
 
 func _ready() -> void:
 	control.modulate.a = 0.0
@@ -27,7 +61,8 @@ func _ready() -> void:
 	player = Thunder._current_player
 	player.completed = true
 	player.change_suit(CharacterManager.get_suit("super"), false, true)
-	#run_while(_smoke_particles, 0.02)
+	run_while(_smoke_particles, 0.02)
+	run_while(_enemy_particles, 0.05, false)
 	
 	await get_tree().create_timer(2.0, false).timeout
 	_destroying = true
@@ -46,22 +81,43 @@ func _ready() -> void:
 
 
 func _physics_process(delta: float) -> void:
-	castle.position.x = castle_pos + randi_range(-5, 5)
+	if skippable:
+		if Input.is_action_pressed(&"m_jump") || Input.is_action_pressed(&"ui_accept"):
+			Scenes.current_scene.end()
+			skippable = false
+	else:
+		castle.position.x = castle_pos + randi_range(-5, 5)
 	
 	if _destroying:
-		_castle_speed -= delta * 50
-		castle.position.y -= delta * _castle_speed
+		_castle_speed += delta * 50
+		castle.position.y += delta * _castle_speed
+	for i in sprites:
+		if !is_instance_valid(i): continue
+		var spd: Vector2 = i.get_meta(&"speed", Vector2.DOWN)
+		i.position += spd * 50 * delta
+		i.rotation += spd.x * 5 * delta
+		i.set_meta(&"speed", spd + Vector2(0, 20) * delta)
+		if i.position.y > 500:
+			i.queue_free()
 	if _flashed:
 		return
 	if castle.position.y > 352:
 		_flash()
+	
+
 
 func _flash() -> void:
 	_flashed = true
 	Audio.play_1d_sound(JEBUT)
 	var tw = create_tween()
 	tw.tween_property(color_rect, "modulate:a", 1.0, 0.25)
-	tw.tween_interval(1.0)
+	tw.tween_callback(func():
+		for i in sprites:
+			if is_instance_valid(i):
+				i.queue_free()
+		sprites = []
+	)
+	tw.tween_interval(2.0)
 	tw.tween_callback(logo_2.show)
 	tw.tween_property(color_rect, "modulate:a", 0.0, 2.0)
 	tw.tween_interval(1.0)
@@ -75,20 +131,21 @@ func text_tw() -> void:
 	tw.tween_callback(func():
 		label.text = credits_text[_label_index]
 		_label_index += 1
-		if _label_index == len(credits_text):
+		if _label_index == len(credits_text) - 1:
 			label.add_theme_font_size_override(&"font_size", 32)
+		elif _label_index == len(credits_text):
+			skippable = true
+			label.remove_theme_font_size_override(&"font_size")
 	)
 	tw.tween_property(label, "modulate:a", 1.0, 0.2)
-	tw.tween_interval(5.0 * Engine.time_scale)
-	await tw.finished
-	Audio.stop_music_channel(1, true)
-	await get_tree().create_timer(2.0, false, true).timeout
-	Scenes.current_scene.end()
+	tw.tween_interval(8.0 * Engine.time_scale)
 
-func run_while(callable: Callable, repeat_delay: float) -> void:
+
+func run_while(callable: Callable, repeat_delay: float, twice: bool = true) -> void:
 	if _flashed: return
 	callable.call()
-	callable.call()
+	if twice:
+		callable.call()
 	await get_tree().create_timer(repeat_delay, false, false, true).timeout
 	run_while(callable, repeat_delay)
 
@@ -100,14 +157,27 @@ func run_while(callable: Callable, repeat_delay: float) -> void:
 	#brick.speed = Vector2(randf_range(-4.0, 4.0), randi_range(-11, -6))
 	#Scenes.current_scene.add_child(brick)
 
+func _enemy_particles() -> void:
+	var d = randi_range(0, spr_dict.keys().size() - 1)
+	
+	var spr = Sprite2D.new()
+	spr.texture = spr_dict.keys()[d]
+	spr.region_enabled = true
+	spr.region_rect = spr_dict.values()[d]
+	var spdx = randi_range(-3, 3)
+	spr.set_meta(&"speed", Vector2(spdx, randi_range(-4, -7)))
+	if spdx == 0:
+		spr.flip_v = true
+	var randx = randi_range(-128, 128)
+	spr.position = Vector2(randx, castle.position.y + 176)
+	if abs(randx) < 48:
+		spr.position.y -= 96
+	sprites.append(spr)
+	sprite_holder.add_child(spr)
 
-#func _smoke_particles() -> void:
-	#var smoke = CASTLE_SMOKE.instantiate()
-	#smoke.position = castle_end_marker.position + Vector2(randi_range(-144, 144), castle.position.y)
-	#smoke.reset_physics_interpolation()
-	#smoke.y_modifier = -20
-	#smoke.y_modify_over_time = 0.4
-	#smoke.apply_force_below_y = 352
-	#smoke.rotation_speed = randi_range(-180, 180)
-	#smoke.z_index = 0
-	#Scenes.current_scene.add_child(smoke)
+
+func _smoke_particles() -> void:
+	var smoke = EXPLOSION_TANK.instantiate()
+	smoke.position = castle_end_marker.position + Vector2(randi_range(-157, 157), 16)
+	smoke.reset_physics_interpolation()
+	Scenes.current_scene.add_child(smoke)
