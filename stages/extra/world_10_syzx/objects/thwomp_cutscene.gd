06ac7a296b8daaeef5fe132441906131ad24eec7
Usered238 <30762980+Usered238@users.noreply.github.com>
2026-05-28 16:10:31 +0900

    fix(syzx w10): broken cutscene; fix(syzx w10): off-center sounds

--- stages/extra/world_10_syzx/objects/thwomp_cutscene.gd
+++ stages/extra/world_10_syzx/objects/thwomp_cutscene.gd
@@ -1,10 +1,10 @@
-extends "res://engine/objects/enemies/thwomp/thwomp.gd"
+extends "res://engine/objects/enemies/thwomp/scripts/thwomp.gd"
 
 func _explosion() -> void:
-	var exp: Node2D = explosion_effect.instantiate()
-	exp.transform = transform
-	exp.translate(left_explosion.position * scale)
-	Scenes.current_scene.add_child(exp)
+	var exp1: Node2D = explosion_effect.instantiate()
+	exp1.transform = transform
+	exp1.translate(left_explosion.position * scale)
+	Scenes.current_scene.add_child(exp1)
 	var exp2: Node2D = explosion_effect.instantiate()
 	exp2.transform = transform
 	exp2.translate(right_explosion.position * scale)
