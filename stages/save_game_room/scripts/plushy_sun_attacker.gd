481ba5c3a0adafd3dd519c98c51e1047ceb85a48
reflexguru <trianglestudioad@gmail.com>
2026-06-15 13:12:53 +0500

    enemies_test

--- stages/save_game_room/scripts/plushy_sun_attacker.gd
+++ stages/save_game_room/scripts/plushy_sun_attacker.gd
@@ -5,5 +5,5 @@ func got_killed(killer_type, special_tags = {}, trigger_enemy_failed_signal = fa
 	$"../..".body_entered()
 	return {}
 
-func got_stomped(by: Node2D, vel: Vector2, offset: Vector2 = Vector2(0, -2)) -> Dictionary:
+func got_stomped(by: Node2D, vel: Vector2, offset: Vector2 = Vector2(0, -16)) -> Dictionary:
 	return {}
