a7ebf0c27dee31d63dea9f2b21965ada43fe55dc
Usered238 <30762980+Usered238@users.noreply.github.com>
2026-06-17 10:34:19 +0900

    fix: -NAN% progress; remove BTE kevin achievement

--- stages/save_game_room/scripts/achievement_pager.gd
+++ stages/save_game_room/scripts/achievement_pager.gd
@@ -41,6 +41,8 @@ func _ready() -> void:
 	total_achievements += classic_page.achievements_number
 	total_unlocked += classic_page.achievements_unlocked
 	completion = (float(total_unlocked) / float(total_achievements)) * 100.0
+	if completion == NAN:
+		completion = 0
 	print("completion: %.2f%%" % completion)
 	label_percent.text %= completion
 
