4eb5be17e0fc366ab3974f0ad09d107620bcd032
Usered238 <30762980+Usered238@users.noreply.github.com>
2026-06-20 23:01:40 +0900

    remove an unused thing which will never be used [no ci]

--- objects/boss_archiwum/projectiles/fly_flame.gd
+++
@@ -1,8 +0,0 @@
-extends "res://engine/objects/enemies/spikes/spike.gd"
-
-var velocity: float
-
-
-func _physics_process(delta: float) -> void:
-	super(delta)
-	global_position += Vector2(velocity, 0).rotated(rotation) * Thunder.get_delta(delta)
