fceaabe22ecc62264e2a35f4c2ce8adc6d09ec85
Usered238 <30762980+Usered238@users.noreply.github.com>
2026-07-05 19:11:06 +0900

    fix: ground in tank levels didn't shake with screen; ci: update to 4.7.1-rc2

--- tilesets/volcano/script_pulsating.gd
+++ tilesets/volcano/script_pulsating.gd
@@ -4,6 +4,10 @@ extends CanvasItem
 
 var counter: float
 
+func _ready() -> void:
+	modulate.v = 1.0 + (1 / divider) + (cos(counter * 3) / divider)
+
+
 func _process(delta: float) -> void:
 	counter += delta
 	modulate.v = 1.0 + (1 / divider) + (cos(counter * 3) / divider)
