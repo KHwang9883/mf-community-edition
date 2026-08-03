271c53fab165dbf6cd15321d14fa096c01268530
Usered238 <30762980+Usered238@users.noreply.github.com>
2026-08-03 10:57:28 +0900

    fix WOS-3 being impossible; feat: WOS mushrooms with wings

--- stages/extra/world_of_stupidity/following_poison_mushroom/following_poison_mushroom.gd
+++ stages/extra/world_of_stupidity/following_poison_mushroom/following_poison_mushroom.gd
@@ -1,4 +1,6 @@
 extends "res://engine/objects/powerups/poisonous_mushroom/poisonous_mushroom.gd"
+@onready var wings: AnimatedSprite2D = get_node_or_null("Wings")
+@onready var wings_2: AnimatedSprite2D = get_node_or_null("Wings2")
 
 func _physics_process(delta: float) -> void:
 	var player: Player = Thunder._current_player
@@ -10,6 +12,9 @@ func _physics_process(delta: float) -> void:
 			global_position = global_position.move_toward(to_pos, delta * 118.75)
 			modulate.a = 1
 			z_index = 5
+			if wings && wings_2:
+				wings.show()
+				wings_2.show()
 		else:
 			appear_process(Thunder.get_delta(delta))
 	
