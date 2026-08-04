a9bd18de008ee5727ce39926cc106c3dbc1e7ba2
Usered238 <30762980+Usered238@users.noreply.github.com>
2026-08-04 18:02:44 +0900

    fix: 1ups behind sign in human lab tower lvl

--- objects/christmas_clones/clone_7/scripts/ice_debris_effect.gd
+++ objects/christmas_clones/clone_7/scripts/ice_debris_effect.gd
@@ -7,6 +7,6 @@ func create() -> void:
 	for i in speeds:
 		NodeCreator.prepare_2d(KEYBOARD_EFFECT, self).create_2d(false).call_method(func(eff: Node2D):
 			eff.global_transform = global_transform
-			eff.position += Vector2(0, 16)
+			eff.position += Vector2(0, 16).rotated(eff.rotation)
 			eff.velocity = i
 		)
