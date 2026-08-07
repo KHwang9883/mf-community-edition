dbb3703b7619540b3730a713419386cb611f981d
Usered238 <30762980+Usered238@users.noreply.github.com>
2026-08-07 15:17:22 +0900

    feat: world u star world for easier difficulties if harder one was completed; remove 1.0/1.1 syzx achievements port code

--- stages/save_game_room/scripts/restore_tweaks_from_minix.gd
+++ stages/save_game_room/scripts/restore_tweaks_from_minix.gd
@@ -24,36 +24,52 @@ func _ready() -> void:
 		Data.values.erase("minix_continue")
 	
 	# Add achievements from old versions
-	old_versions_achievements_patch()
+	#old_versions_achievements_patch()
 
 	if !SettingsManager.get_custom_setting("force_enable_deprecated_tweaks", false):
 		if SettingsManager.get_tweak("copyright_free_ost"):
 			SettingsManager.set_tweak("copyright_free_ost", false)
 
 
-func old_versions_achievements_patch() -> void:
-	set_syzx_secret("syzx worlds completed",
-		"syzxchulun world 9",
-		"syzxchulun world 10"
-	)
-	set_syzx_secret("syzx worlds secret mode",
-		"syzxchulun world 9 kevin mode",
-		"syzxchulun world 10 kevin mode"
-	)
-	set_syzx_secret("syzx worlds completed softendo",
-		"syzxchulun world 9 softendo",
-		"syzxchulun world 10 advance"
-	)
-	set_syzx_secret("syzx worlds secret mode softendo",
-		"syzxchulun world 9 softendo kevin mode",
-		"syzxchulun world 10 advance kevin mode"
-	)
+#func old_versions_achievements_patch() -> void:
+	#set_syzx_secret("syzx worlds completed",
+		#"syzxchulun world 9",
+		#"syzxchulun world 10"
+	#)
+	#set_syzx_secret("syzx worlds secret mode",
+		#"syzxchulun world 9 kevin mode",
+		#"syzxchulun world 10 kevin mode"
+	#)
+	#set_syzx_secret("syzx worlds completed softendo",
+		#"syzxchulun world 9 softendo",
+		#"syzxchulun world 10 advance"
+	#)
+	#set_syzx_secret("syzx worlds secret mode softendo",
+		#"syzxchulun world 9 softendo kevin mode",
+		#"syzxchulun world 10 advance kevin mode"
+	#)
+#
+#func set_syzx_secret(secret_name: String, w9: String, w10: String) -> void:
+	#var arr_1 = SecretsManager.get_secret(secret_name)
+	#if arr_1 && arr_1 is Array:
+		#if "9" in arr_1 && !SecretsManager.has_secret(w9):
+			#SecretsManager.set_secret(w9, true, true, false)
+		#if "10" in arr_1 && !SecretsManager.has_secret(w10):
+			#SecretsManager.set_secret(w10, true, true, false)
+	#
 
-func set_syzx_secret(secret_name: String, w9: String, w10: String) -> void:
-	var arr_1 = SecretsManager.get_secret(secret_name)
-	if arr_1 && arr_1 is Array:
-		if "9" in arr_1 && !SecretsManager.has_secret(w9):
-			SecretsManager.set_secret(w9, true, true, false)
-		if "10" in arr_1 && !SecretsManager.has_secret(w10):
-			SecretsManager.set_secret(w10, true, true, false)
+## Hard diff open
+func _on_pipe_in_world_uh_star_world_available() -> void:
+	_open_star_world_for(get_node_or_null(^"../Objects/PipeInWorldUE"))
+	_open_star_world_for(get_node_or_null(^"../Objects/PipeInWorldUN"))
+
+## Normal diff open
+func _on_pipe_in_world_un_star_world_available() -> void:
+	_open_star_world_for(get_node_or_null(^"../Objects/PipeInWorldUE"))
+
+func _open_star_world_for(warp: Area2D) -> void:
+	if is_instance_valid(warp) && !warp._star_world:
+		warp._star_world = true
+		warp._do_not_block = true
+		warp.label.set_world_numbers("%d-0" % warp._star_sel_level)
 	
