8d11a3b657ce091ffb87fa05ec5361fa04b954df
Usered238 <30762980+Usered238@users.noreply.github.com>
2026-08-08 15:39:03 +0900

    optimize rain particles

--- gfx/rain/rain.gd
+++ gfx/rain/rain.gd
@@ -1,6 +1,8 @@
 extends GPUParticles2D
 
 @export_category("Rain")
+@export var snap_to_camera: bool = true
+@export var no_y_axis_snap: bool = false
 @export_group("Sound", "sound_")
 @export_range(-50, 50, 0.1, "or_less", "or_greater", "suffix:dB") var sound_basic_volume: float = -6
 @export_range(-50, 50, 0.1, "or_less", "or_greater", "suffix:dB") var sound_dying_volume: float = -100.0
@@ -17,8 +19,36 @@ var _tweening: bool = false
 
 
 func _ready() -> void:
+	var cam: PlayerCamera2D = Thunder._current_camera
+	if cam && snap_to_camera:
+		physics_interpolation_mode = Node.PHYSICS_INTERPOLATION_MODE_OFF
+		cam.physics_interpolation_reset.connect(func() -> void:
+			var screen_center: Vector2 = cam.get_screen_center_position() + Vector2(0, -240)
+			var old_pos := global_position
+			if no_y_axis_snap:
+				global_position.x = screen_center.x
+				old_pos.y = screen_center.y
+			else:
+				global_position = screen_center
+			if old_pos.distance_squared_to(screen_center) > 480 ** 2:
+				restart()
+		)
+	if !sound_only_in_visible_rect:
+		_sound.volume_db = sound_basic_volume
 	SettingsManager.settings_updated.connect(_update_visibility, CONNECT_DEFERRED)
 	_update_visibility.call_deferred()
+
+
+func _physics_process(delta: float) -> void:
+	if !snap_to_camera:
+		return
+	var cam: PlayerCamera2D = Thunder._current_camera
+	if !cam: return
+	if no_y_axis_snap:
+		global_position.x = cam.get_screen_center_position().x
+	else:
+		global_position = cam.get_screen_center_position() + Vector2(0, -240)
+
 
 func _process(_delta: float) -> void:
 	if !sound_only_in_visible_rect:
