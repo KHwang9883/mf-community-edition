fbbca46a17c8e8a328551532e2d63716efbc76e2
SerafimGWS <157095079+SerafimGWS@users.noreply.github.com>
2026-09-15 00:05:46 +0200

    heavy skies progress

--- commands/heavyskies.gd
+++ commands/heavyskies.gd
@@ -2,6 +2,23 @@ extends Command
 
 const WIND_NODE = preload("res://stages/extra/hardcore_2/objects/spike_roof.tscn")
 #const WIND_FX_NODE = preload("res://stages/extra/expert_mode/objects/world4_3wind_fx.tscn")
+const SCENE_PATHS_1: Array[StringName] = [
+	&"level_1-3",
+	&"expert_level_1-3",
+	&"level_5-2",
+	&"level_8-4_boss",
+	&"expert_level_8-4_boss",
+	&"level_f1-4",
+	&"level_f2-2",
+	&"level_f2-4",
+	&"level_f3-3",
+	&"level_f4-4",
+	&"level_u-3",
+	&"level_u-4-3",
+	&"level_u-4_e",
+	&"level_u-4_n",
+	&"level_u-4_h",
+]
 
 static func register() -> Command:
 	return new().set_name("heavyskies").set_description("Make every level a Hardcore 2-2")
@@ -27,5 +44,9 @@ func patch_level() -> void:
 	var spawner = WIND_NODE.instantiate()
 	#var spawner2 = WIND_FX_NODE.instantiate()
 	var scene_path: String = Scenes.current_scene.scene_file_path
+	if SCENE_PATHS_1.any(func(path: StringName):
+		return path in scene_path
+	):
+		spawner.get_child(0).bottom_line_position = 288.0
 	Scenes.current_scene.add_child(spawner)
 	#Scenes.current_scene.add_child(spawner2)
